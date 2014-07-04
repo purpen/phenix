94d76db5b956930b3b7153ea1c60c4581027fcc6
purpen <purpen.w@gmail.com>
2014-07-04 18:26:53 +0800

    debug wechat address.

--- src/Sher/App/Action/Wxpay.php
+++ src/Sher/App/Action/Wxpay.php
@@ -3,19 +3,74 @@
  * 微信支付相关接口
  * @author purpen
  */
-class Sher_App_Action_Wxpay extends Sher_App_Action_Base {
+class Sher_App_Action_Wxpay extends Sher_App_Action_Base implements DoggyX_Action_Initialize {
+	
 	public $stash = array(
-		
+		'id'=>'',
+		'rid' => 0,		
+		'openid' => '',
+		'code' => '',
 	);
 	
-	protected $exclude_method_list = array('execute');
-
+	// 配置微信参数
+	public $options = array();
+	
+	protected $exclude_method_list = array('execute','addr','payment','direct_native','warning', 'feedback');
+	
+	/**
+	 * 初始化参数
+	 */
+	public function _init() {
+		$this->options = array(
+			'token'=>Doggy_Config::$vars['app.wechat.ser_token'],
+			'appid'=>Doggy_Config::$vars['app.wechat.ser_app_id'],
+			'appsecret'=>Doggy_Config::$vars['app.wechat.ser_app_secret'],
+			'partnerid'=>Doggy_Config::$vars['app.wechat.ser_partner_id'],
+			'partnerkey'=>Doggy_Config::$vars['app.wechat.ser_partner_key'],
+			'paysignkey'=>Doggy_Config::$vars['app.wechat.ser_paysignkey'] //商户签名密钥Key
+		);
+    }
+	
 	/**
 	 * 默认入口
 	 */
 	public function execute(){
 		return $this->payment();
 	}
+	
+	/**
+	 * 获取用户共享收货地址
+	 */
+	public function addr(){
+		$wechat = new Sher_Core_Util_Wechat($this->options);
+		
+		$timestamp = time();
+		$noncestr = $wechat->generateNonceStr();
+		
+		$user_id = '469';
+		
+		// 微信共享地址参数
+		$wxaddr_options = array(
+			'appId' => $this->options['appid'],
+			'timeStamp' => $timestamp,
+			'nonceStr' => $noncestr,
+		);
+		
+		$redis = new Sher_Core_Cache_Redis();
+		$access_json = $redis->get('weixin_'.$user_id.'_oauth_access');
+		if (!empty($access_json)){
+			$addrsign = $wechat->getAddrSign($current_url, $timestamp, $noncestr, $access_json['access_token']);
+			
+			$wxaddr_options['addrSign'] = $addrsign;
+		}
+		
+		$this->stash['wxaddr_options'] = $wxaddr_options;
+		
+		return $this->to_html_page('page/wechat/addr.html');
+	}
+	
+	
+	
 	
 	/**
 	 * 微信支付请求实例
