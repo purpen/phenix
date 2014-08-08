96c5b066dd8e3e467cbd2d98639b70133b3f9f83
purpen <purpen.w@gmail.com>
2014-08-08 20:54:23 +0800

    debug upload.

--- src/Sher/Core/Helper/View.php
+++ src/Sher/Core/Helper/View.php
@@ -14,6 +14,7 @@ class Sher_Core_Helper_View {
         $stash['js_jquery_bundle_version'] = Doggy_Config::$vars['app.version.jquery_bundle_version'];
         $stash['css_use_bundle'] = Doggy_Config::$vars['app.css.use_bundle'];
         $stash['css_bundle_version'] = Doggy_Config::$vars['app.version.css_bundle_version'];
+		$stash['app_mode'] = Doggy_Config::$vars['app.mode'];
 	}
     /**
      * 设置主菜单url
