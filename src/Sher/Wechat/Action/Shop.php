9438ef086e5c89633afd247ddd1355262d427dd5
purpen <purpen.w@gmail.com>
2014-08-09 13:42:53 +0800

    add zhizao.

--- src/Sher/Wechat/Action/Shop.php
+++ src/Sher/Wechat/Action/Shop.php
@@ -29,7 +29,7 @@ class Sher_Wechat_Action_Shop extends Sher_App_Action_Base implements DoggyX_Act
 	// 配置微信参数
 	public $options = array();
 	
-	protected $exclude_method_list = array('execute','featured','newest','get_list','view');
+	protected $exclude_method_list = array('execute','featured','newest','get_list','view', 'z');
 	
 	public function _init() {
 		$this->options = array(
