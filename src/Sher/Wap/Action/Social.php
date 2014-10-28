9ab5cf3dd879f165672124d74e90ed1eba98824f
purpen <purpen.w@gmail.com>
2014-10-28 18:18:48 +0800

    update wap match.

--- src/Sher/Wap/Action/Social.php
+++ src/Sher/Wap/Action/Social.php
@@ -23,7 +23,23 @@ class Sher_Wap_Action_Social extends Sher_Core_Action_Authorize {
 	 * 十万火计
 	 */
 	public function dream(){
+		$this->stash['dream_category_id'] = Doggy_Config::$vars['app.topic.dream_category_id'];
 		return $this->to_html_page('wap/match.html');
+	}
+	
+	/**
+	 * 全部创意列表
+	 */
+	public function allist(){
+		$this->set_target_css_state('allist');
+		
+		$page = "?page=#p#";
+		$pager_url = Sher_Core_Helper_Url::build_url_path('app.url.social', 'allist').$page;
+		$this->stash['pager_url'] = $pager_url;
+		
+		$this->stash['dream_category_id'] = Doggy_Config::$vars['app.topic.dream_category_id'];
+		
+		return $this->to_html_page('wap/match_list.html');
 	}
 	
 	
