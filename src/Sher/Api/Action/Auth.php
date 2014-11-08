315f0c61efb6ef05ddcd470eedf81c95ed67bc15
purpen <purpen.w@gmail.com>
2014-11-08 13:57:17 +0800

    debug api register.

--- src/Sher/Api/Action/Auth.php
+++ src/Sher/Api/Action/Auth.php
@@ -66,23 +66,23 @@ class Sher_Api_Action_Auth extends Sher_Api_Action_Base {
 		$this->resparams = array_merge($this->resparams, array('mobile','password','verify_code'));
 		// 验证请求签名
 		if(Sher_Core_Helper_Util::get_signature($this->stash, $this->resparams, $this->client_id) != $this->sign){
-			return $this->api_json('数据错误,请重试!', 300);
+			return $this->api_json('数据错误,请重试!', 3000);
 		}
 		
 	    if (empty($this->stash['mobile']) || empty($this->stash['password']) || empty($this->stash['verify_code'])) {
-            return $this->ajax_json('数据错误,请重试!', true);
+            return $this->api_json('数据错误,请重试!', 3001);
         }
 		
 		// 验证手机号码格式
 		if(!Sher_Core_Helper_Util::is_mobile($this->stash['mobile'])){
-			return $this->ajax_json('手机号码格式不正确!', true);
+			return $this->api_json('手机号码格式不正确!', 3002);
 		}
 		
 		// 验证验证码是否有效
 		$verify = new Sher_Core_Model_Verify();
 		$code = $verify->first(array('phone'=>$this->stash['mobile'],'code'=>$this->stash['verify_code']));
 		if(empty($code)){
-			return $this->ajax_json('验证码有误，请重新获取！', true);
+			return $this->api_json('验证码有误，请重新获取！', 3004);
 		}
 		
 		$user_info = array(
@@ -121,10 +121,10 @@ class Sher_Api_Action_Auth extends Sher_Api_Action_Base {
 			}
         }catch(Sher_Core_Model_Exception $e){
             Doggy_Log_Helper::error('Failed to register:'.$e->getMessage());
-            return $this->ajax_json("注册失败:".$e->getMessage(), true);
+            return $this->api_json($e->getMessage(), 4001);
         }
 		
-		return $this->ajax_json("注册成功，欢迎加入太火鸟！", false, null, $visitor);
+		return $this->api_json("注册成功，欢迎加入太火鸟！", 0, $visitor);
 	}
 	
 	/**
