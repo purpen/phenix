2668a0ff530d71ee36d5f56dacd1ec3a421669df
tianshuai <tianshuaiorc@sina.com>
2014-11-19 10:43:47 +0800

    init

---
+++ vendor/purpen/qiniu/src/Qiniu.php
@@ -0,0 +1,15 @@
+<?php
+namespace Qiniu;
+
+class Qiniu
+{
+    /**
+     * Factory a client
+     *
+     * @param array $options
+     * @return Client
+     */
+    public static function create(array $options){
+        return new Client($options);
+    }
+}
