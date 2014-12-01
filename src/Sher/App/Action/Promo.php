84a9e33bd8c8f5733925583759a4f35c5d4fe230
purpen <purpen.w@gmail.com>
2014-12-01 21:10:58 +0800

    update promo page.

--- src/Sher/App/Action/Promo.php
+++ src/Sher/App/Action/Promo.php
@@ -24,5 +24,32 @@ class Sher_App_Action_Promo extends Sher_App_Action_Base {
 		return $this->to_html_page('page/tweleve.html');
 	}
 	
+	/**
+	 * 获取红包
+	 */
+	public function bonus(){
+		$bonus = new Sher_Core_Model_Bonus();
+		$result = $bonus->pop();
+		
+		# 获取为空，重新生产红包
+		while(empty($result)){
+			$bonus->create_batch_bonus(100);
+			$result = $bonus->pop();
+			// 跳出循环
+			if(!empty($result)){
+				break;
+			}
+		}
+		
+		# 返回结果
+		$data = array(
+			'code' => $result['code'],
+			'amount' => $result['amount'],
+		);
+		
+		return $this->to_taconite_page('ajax/bonus_ok.html');
+	}
+	
+	
 }
 ?>
