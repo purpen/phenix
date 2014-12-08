b83b4643601a20f23345b1f224cf3d80347071f0
lyf-THN <987083180@qq.com>
2014-12-08 17:45:46 +0800

    merge

--- bin/fixed_get_user.php
+++ bin/fixed_get_user.php
@@ -13,7 +13,7 @@ include $drr;
 //require_once dirname(__FILE__).'/PHPExcel/Classes/PHPExcel.php';
 
 
-$config_file =  dirname(__FILE__).'/../deploy/app_config.php';
+$config_file =  dirname(__FILE__).'/../deploy/app_config.php.example';
 if (!file_exists($config_file)) {
     die("Can't find config_file: $config_file\n");
 }
@@ -31,7 +31,7 @@ ini_set('memory_limit','512M');
 
 echo "Prepare to get user...\n";
 
-/*try{
+try{
     $user = new Sher_Core_Model_User();
     $ok = $user->find();	
 	$objPHPExcel=new PHPExcel();
@@ -72,44 +72,6 @@ echo "Prepare to get user...\n";
 	exit;
 
     }catch(Sher_Core_Model_Exception $e){
-	echo "Get the user failed: ".$e->getMessage();
-		
-	continue;
-
-   }*/
-try{
-    $user = new Sher_Core_Model_User();
-    $ok = $user->find();
-	header('Content-Type: application/vnd.ms-excel'); 
-	header('Content-Disposition: attachment;filename="user.csv"'); 
-	header('Cache-Control: max-age=0');
-	$fp = fopen('php://output','a');
-	$head = array('姓名', '性别', '年龄', 'Email', '电话', '……'); 
-	foreach ($head as $i => $v) { 
-	// CSV的Excel支持GBK编码，一定要转换，否则乱码 
-	$head[$i] = iconv('utf-8', 'gbk', $v); 
-	}
-	fputcsv($fp, $head);
-	$cnt = 0; 
-	// 每隔$limit行，刷新一下输出buffer，不要太大，也不要太小 
-	$limit = 100000;
-	// 逐行取出数据，不浪费内存 
-	while ($row = $ok) {
-	$cnt ++; 
-	if ($limit == $cnt) { //刷新一下输出buffer，防止由于数据过多造成问题 
-	ob_flush(); 
-	flush(); 
-	$cnt = 0; 
-	}
-	foreach ($row as $i => $v) { 
-	$row[$i] = iconv('utf-8', 'gbk', $v); 
-	} 
-	fputcsv($fp, $row); 
-	}
-
-
-
-	}catch(Sher_Core_Model_Exception $e){
 	echo "Get the user failed: ".$e->getMessage();
 		
 	continue;
