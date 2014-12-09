f6cc74ad3d43677df3420f4fb7e42af38a9233a5
TianShuai <tianshuaiorc@sina.com>
2014-12-09 22:03:32 +0800

    validate nickname

--- src/Sher/Core/Model/User.php
+++ src/Sher/Core/Model/User.php
@@ -212,20 +212,31 @@ class Sher_Core_Model_User extends Sher_Core_Model_Base {
 	/**
 	 * 检查昵称是否唯一
 	 */
-	public function _check_name($nickname=null) {
+	public function _check_name($nickname=null, $user_id=0) {
 		if (is_null($nickname)){
       $nickname = $this->data['nickname'];
 		}
 		if(empty($nickname)){
 			return false;
 		}
-    $user_id = $this->data['_id'];
+
 		Doggy_Log_Helper::debug("Validate user name[$nickname]!");
-		$row = $this->first(array('nickname' => $nickname, '_id' => array('$ne'=>$user_id)));
-		if(!empty($row)){
-			return false;
-		}
-		return true;
+		$rows = $this->find(array('nickname' => $nickname));
+		if(empty($rows)){
+			return true;
+    }else{
+      //判断是否更新状态
+      if($user_id != 0){
+        if(count($rows)==1){
+          if($rows[0]['_id']==$user_id) return true;
+          return false;
+        }else{
+          return false;
+        }
+      }else{
+        return false;
+      }
+    }
 	}
 	
 	/**
