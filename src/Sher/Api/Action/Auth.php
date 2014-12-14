cbfb2c3f9ad6bf930e27606a8e08361ebfe4339e
purpen <purpen.w@gmail.com>
2014-12-14 17:43:09 +0800

    update api user info.

--- src/Sher/Api/Action/Auth.php
+++ src/Sher/Api/Action/Auth.php
@@ -218,7 +218,7 @@ class Sher_Api_Action_Auth extends Sher_Api_Action_Base {
 		$model = new Sher_Core_Model_User();
 		$result = $model->extend_load((int)$id);
 		
-		$some_fields = array('_id'=>1,'account'=>1,'nickname'=>1,'medium_avatar_url'=>1,'state'=>1,'first_login'=>1,'profile'=>1,'city'=>1,'sex'=>1,'summary'=>1,'created_on'=>1);
+		$some_fields = array('_id'=>1,'account'=>1,'nickname'=>1,'state'=>1,'first_login'=>1,'profile'=>1,'city'=>1,'sex'=>1,'summary'=>1,'created_on'=>1);
 		
 		// 重建数据结果
 		$data = array();
@@ -226,6 +226,8 @@ class Sher_Api_Action_Auth extends Sher_Api_Action_Base {
 			$data[$key] = $result[$key];
 		}
 		$data['phone'] = $result['profile']['phone'];
+		$data['job'] = $result['profile']['job'];
+		$data['avatar'] = $result['medium_avatar_url'];
 		
 		$result = $data;
 		
