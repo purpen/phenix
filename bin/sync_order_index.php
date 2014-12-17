32e4e4acc6c1682e8614d4531b8114ed6c5e530d
purpen <purpen.w@gmail.com>
2014-12-17 20:53:54 +0800

    update sync script.

--- bin/sync_order_index.php
+++ bin/sync_order_index.php
@@ -21,14 +21,16 @@ require $cfg_doggy_bootstrap;
 set_time_limit(0);
 ini_set('memory_limit','512M');
 
-echo "Prepare to fix topic try_id...\n";
+echo "Prepare to fix order status ...\n";
 
 $order = new Sher_Core_Model_Orders();
 $order_index = new Sher_Core_Model_OrdersIndex();
 $page = 1;
 $size = 1000;
 $is_end = false;
+
 $total = 0;
+
 while(!$is_end){
 	$query = array();
 	$options = array('field'=>array('_id','rid','status'), 'page'=>$page, 'size'=>$size);
@@ -39,28 +41,29 @@ while(!$is_end){
 	}
 	$max = count($list);
 	for ($i=0; $i<$max; $i++) {
-    $order_id = $list[$i]['_id'];
+    	$order_id = $list[$i]['_id'];
 		$status = $list[$i]['status']; 
-    $rid = $list[$i]['rid'];
+    	$rid = $list[$i]['rid'];
 
-    $data = $order_index->first(array('order_id'=>$order_id));
-    if(empty($data)){
-      continue;
-    }
-    if($data['status']==$status){
-      continue;
-    }
-    $order_index_id = $data['_id'];
-    $ok = $order_index->update_set($order_index_id, array('status'=>$status));
-    if($ok){
- 		  echo "sync success order_index [".(string)$rid."]..........\n";   
-    }
+    	$data = $order_index->first(array('order_id'=>$order_id));
+    	if(empty($data)){
+      	    continue;
+    	}
+    	if($data['status'] == $status){
+      	    continue;
+    	}
+    	$order_index_id = $data['_id'];
+    	$ok = $order_index->update_set($order_index_id, array('status'=>$status));
+    	if($ok){
+ 		  	echo "sync success order_index [".(string)$rid."]..........\n";
+    	}
 		
 		$total++;
 		
 		unset($data);
 	}
 	if($max < $size){
+		$is_end = true;
 		echo "order list is end!!!!!!!!!,exit.\n";
 		break;
 	}
@@ -68,5 +71,5 @@ while(!$is_end){
 	echo "page [$page] updated---------\n";
 }
 
-echo "sync is OK! \n";
+echo "sync [$total] is OK! \n";
 ?>
