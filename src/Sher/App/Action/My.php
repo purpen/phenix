c44bf2d78e6ac804c5ad8165c0a52ba1585b9449
TianShuai <tianshuaiorc@sina.com>
2014-12-23 10:31:36 +0800

    refund

--- src/Sher/App/Action/My.php
+++ src/Sher/App/Action/My.php
@@ -114,6 +114,9 @@ class Sher_App_Action_My extends Sher_App_Action_Base implements DoggyX_Action_I
 			case 1:
 				$this->set_target_css_state('nopayed');
 				break;
+      case 2:
+        $this->set_target_css_state('ready_goods');
+        break;
 			case 9: // 已关闭订单：取消的订单、过期的订单
 				$this->set_target_css_state('closed');
 				break;
