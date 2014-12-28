efe65329266a90596bd458d989abbafc2d552b58
TianShuai <tianshuaiorc@sina.com>
2014-12-28 19:58:16 +0800

    playegg

--- src/Sher/Wap/Action/Index.php
+++ src/Sher/Wap/Action/Index.php
@@ -49,7 +49,10 @@ class Sher_Wap_Action_Index extends Sher_Wap_Action_Base {
 	 * 游戏
 	 */
 	public function games(){
-		return $this->to_html_page('wap/games.html');
+    $playegg_url = Doggy_Config::$vars['app.url.wap'].'/playegg/index.html';
+    //直接跳转游戏页面
+    return $this->to_redirect($playegg_url);
+		//return $this->to_html_page('wap/games.html');
 	}
 	
 	/**
