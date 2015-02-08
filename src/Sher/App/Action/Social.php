289dfa34e2905bc00a1b1a818a31d42d023c2041
TianShuai <tianshuaiorc@sina.com>
2015-02-08 19:27:43 +0800

    match

--- src/Sher/App/Action/Social.php
+++ src/Sher/App/Action/Social.php
@@ -15,7 +15,7 @@ class Sher_App_Action_Social extends Sher_App_Action_Base implements DoggyX_Acti
 	protected $page_tab = 'page_sns';
 	protected $page_html = 'page/social/index.html';
 	
-	protected $exclude_method_list = array('execute','dream','allist','dream2','about2');
+	protected $exclude_method_list = array('execute','dream','allist','allist2','dream2','about2');
 	
 	public function _init() {
 		$this->set_target_css_state('page_social');
@@ -59,7 +59,9 @@ class Sher_App_Action_Social extends Sher_App_Action_Base implements DoggyX_Acti
 	/**
 	 * 十万火计--第二季 活动介绍
 	 */
-	public function about2() {		
+  public function about2() {
+		$this->set_target_css_state('about');
+		$this->stash['dream_category_id'] = Doggy_Config::$vars['app.topic.dream2_category_id'];
 		return $this->to_html_page('match/about2.html');
 	}
 	
@@ -82,6 +84,23 @@ class Sher_App_Action_Social extends Sher_App_Action_Base implements DoggyX_Acti
 		return $this->to_html_page('match/list.html');
 	}
 	
+	/**
+	 * 十万火计--第二季 列表页
+	 */
+  public function allist2() {
+		$this->set_target_css_state('allist');
+		$sort = $this->stash['st'];
+		
+		$page = "?st=${sort}&page=#p#";
+		$pager_url = Sher_Core_Helper_Url::build_url_path('app.url.social', 'allist2').$page;
+		$this->stash['pager_url'] = $pager_url;
+		
+		$this->stash['dream_category_id'] = Doggy_Config::$vars['app.topic.dream2_category_id'];
+		
+		$this->stash['start_time'] = mktime(0,0,0,10,28,2014);
+		$this->stash['end_time'] = mktime(23,59,59,12,20,2014);
+		return $this->to_html_page('match/list2.html');
+	}
 	
 	/**
 	 * 社区列表
