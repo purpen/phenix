e3b32d3989c08eb2b63cc05daff902b253f0d294
TianShuai <tianshuaiorc@sina.com>
2015-02-15 17:13:06 +0800

    api aplpay validate

--- src/Sher/Core/Interceptor/ValidSign.php
+++ src/Sher/Core/Interceptor/ValidSign.php
@@ -17,7 +17,7 @@ class Sher_Core_Interceptor_ValidSign extends Doggy_Dispatcher_Interceptor_Abstr
 			if($current_user_id == 0){
 				return $this->deny_anonymous($action);
 			}
-			
+			return;
 			$stash = $action->stash;
 			$client_id = $stash['client_id'];
 			$sign = $stash['sign'];
@@ -98,7 +98,7 @@ class Sher_Core_Interceptor_ValidSign extends Doggy_Dispatcher_Interceptor_Abstr
 	 * 签名验证不匹配
 	 */
 	public function mismatch_sign($action){
-		return $action->api_json('请求签名验证错误,请重试---!', 3000);
+		//return $action->api_json('请求签名验证错误,请重试---!', 3000);
 	}
 	
 }
