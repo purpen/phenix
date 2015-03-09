b0915851296422aa5365761fd71be6e270533ac0
Pan Fan <nightsailer@gmail.com>
2015-03-09 17:43:54 +0800

    fix session id leak generated rule.

--- doggy/doggy-x/src/DoggyX/Session/Engine/Mongo.php
+++ doggy/doggy-x/src/DoggyX/Session/Engine/Mongo.php
@@ -20,13 +20,24 @@ class DoggyX_Session_Engine_Mongo extends DoggyX_Model_Mongo_Base implements Dog
     public $ttl = 3600;
     
     protected $opened = false;
-    
-    
-    public function __construct($ttl=3600,$options=array()) {
+
+    protected $secret_key = false;
+
+    const BLANK_SECRET_KEY = 'CHANGE_IT';
+
+    public function __construct($ttl=3600, $options=array()) {
         $this->ttl = $ttl;
         // custom collection name
         if (!empty($options['collection'])) {
             $this->collection = $options['collection'];
+        }
+        if (!empty(Doggy_Config::$vars['app.session.secret_key'])) {
+            $this->secret_key = Doggy_Config::$vars['app.session.secret_key'];
+            if ($this->secret_key == $this::BLANK_SECRET_KEY) {
+                throw new DoggyX_Session_Exception('Session secret_key is initial value, you MUST manual set it in <config/session.yml>');
+            }
+        } else {
+            throw new DoggyX_Session_Exception('Session secret_key is NULL, you MUST set it in <config/session.yml>');
         }
         parent::__construct();
     }
@@ -41,7 +52,7 @@ class DoggyX_Session_Engine_Mongo extends DoggyX_Model_Mongo_Base implements Dog
             throw new DoggyX_Session_Exception('session is started, please close it first!');
         }
         $this->reset();
-        $data['_id'] = DoggyX_Mongo_Db::id($sid);
+        $data['_id'] = $this->build_session_id();
         $data['created_on'] = $data['alive'] = time();
         $this->create($data);
         $this->opened = true;
@@ -81,7 +92,14 @@ class DoggyX_Session_Engine_Mongo extends DoggyX_Model_Mongo_Base implements Dog
     }
     
     public function build_session_id() {
-        return (string) new MongoId();
+        $sid_data = (string) new MongoId();
+        // if hash extension installed, use it first, or fallback to pure php implements
+        if (function_exists('hash_hmac')) {
+            $key = hash_hmac('sha1', $sid_data, $this->secret_key);
+        } else {
+            $key = Doggy_Util_Crypt_Util::hmac_sha1($this->secret_key, $sid_data);
+        }
+        return $key;
     }
 }
 ?>
