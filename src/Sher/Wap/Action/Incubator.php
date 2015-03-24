02294c3f584ca45fa0b29c4bb9e0e4fcf582e17f
TianShuai <tianshuaiorc@sina.com>
2015-03-24 16:10:24 +0800

    wap Incubator

---
+++ src/Sher/Wap/Action/Incubator.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * 孵化器
+ * @author tianshuai
+ */
+class Sher_Wap_Action_Incubator extends Sher_App_Action_Base implements DoggyX_Action_Initialize {
+	
+	public $stash = array(
+		
+	);
+	
+	protected $exclude_method_list = array('execute','index');
+	
+	public function _init() {
+		//$this->set_target_css_state('page_incubator');
+    }
+	
+	/**
+	 * 默认入口
+	 */
+	public function execute(){
+		return $this->index();
+	}
+	
+	/**
+	 * 首页
+	 */
+	public function index(){		
+		return $this->to_html_page('wap/incubator/index.html');
+	}
+	
+  	/**
+   	 * 产品合作表单提交
+     */
+    public function cooperate(){
+	  	$row = array();
+	    $this->stash['mode'] = 'create';
+
+	    $callback_url = Doggy_Config::$vars['app.url.qiniu.onelink'];
+	    $this->stash['editor_token'] = Sher_Core_Util_Image::qiniu_token($callback_url);
+	    $this->stash['editor_domain'] = Sher_Core_Util_Constant::STROAGE_ASSET;
+
+	    $this->stash['token'] = Sher_Core_Util_Image::qiniu_token();
+	    $this->stash['pid'] = new MongoId();
+    
+	    $this->stash['asset_type'] = Sher_Core_Model_Asset::TYPE_CONTACT;
+
+		$this->stash['contact'] = $row;
+		
+	    return $this->to_html_page('wap/cooperate/cooperate_submit.html');
+  	}
+	
+    /**
+     * 产品合作保存
+     */
+    public function save_cooperate(){
+		// 验证数据
+		if(empty($this->stash['category_id'])){
+			return $this->ajax_json('请选择一个分类！', true);
+		}
+		if(empty($this->stash['title'])){
+			return $this->ajax_json('产品名称不能为空！', true);
+		}
+		if(empty($this->stash['content'])){
+			return $this->ajax_json('产品详情不能为空！', true);
+		}
+		if(empty($this->stash['name'])){
+			return $this->ajax_json('联系人不能为空！', true);
+		}
+		if(empty($this->stash['tel'])){
+			return $this->ajax_json('联系电话不能为空！', true);
+		}
+		if(empty($this->stash['email'])){
+			return $this->ajax_json('邮箱不能为空！', true);
+		}
+		
+		$id = (int)$this->stash['_id'];
+		
+		//保存信息
+		$data = array();
+		$data['title'] = $this->stash['title'];
+		$data['category_id'] = $this->stash['category_id'];
+		$data['content'] = $this->stash['content'];
+		$data['name'] = $this->stash['name'];
+		$data['tel'] = $this->stash['tel'];
+		$data['email'] = $this->stash['email'];
+
+		$data['cover_id'] = $this->stash['cover_id'];
+		// 检查是否有附件
+		if(isset($this->stash['asset'])){
+			$data['asset'] = $this->stash['asset'];
+			$data['asset_count'] = count($data['asset']);
+		}else{
+			$data['asset'] = array();
+			$data['asset_count'] = 0;
+		}
+		
+		try{
+			$model = new Sher_Core_Model_Contact();
+			
+			// 新建记录
+			if(empty($id)){
+				$data['user_id'] = (int)$this->visitor->id;
+					
+				$ok = $model->apply_and_save($data);
+				
+			}else{
+				$data['_id'] = $id;
+				
+				$ok = $model->apply_and_update($data);
+			}
+			
+			if(!$ok){
+				return $this->ajax_json('保存失败,请重新提交', true);
+			}
+			
+		}catch(Sher_Core_Model_Exception $e){
+			return $this->ajax_json('产品合作保存失败:'.$e->getMessage(), true);
+		}
+
+    	$this->stash['is_error'] = false;
+    	$this->stash['note'] = '保存成功!';
+		$this->stash['redirect_url'] = Doggy_Config::$vars['app.url.incubator'];
+		
+		return $this->to_taconite_page('ajax/note.html');
+    }
+	
+}
+?>
