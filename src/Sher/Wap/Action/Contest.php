2dd7ef8e69d41c7f6e4808c58ecec4dedd7f7561
TianShuai <tianshuaiorc@sina.com>
2015-04-03 14:18:13 +0800

    contest

--- src/Sher/Wap/Action/Contest.php
+++ src/Sher/Wap/Action/Contest.php
@@ -123,7 +123,7 @@ class Sher_Wap_Action_Contest extends Sher_Wap_Action_Base {
     $query['type'] = Sher_Core_Model_SumRecord::TYPE_PRO;
     $options['page'] = 1;
     $options['size'] = 6;
-    $options['sort'] = array('match2_love_count'=> -1);
+    $options['sort'] = array('match2_count'=> -1);
     $data = $model->find($query, $options);
     foreach($data as $key=>$val){
       $pid = (int)$data[$key]['target_id'];
