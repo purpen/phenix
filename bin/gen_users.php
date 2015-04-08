22be72dac5d4745023c431772f4839ea292dedb5
purpen <purpen.w@gmail.com>
2015-04-08 14:01:14 +0800

    update gen users script

--- bin/gen_users.php
+++ bin/gen_users.php
@@ -20,51 +20,55 @@ require $cfg_doggy_bootstrap;
 
 set_time_limit(0);
 
-
 // 批量创建账号
-function batch_create_user(){
+function batch_create_user($start=1000){
 	echo "Create batch the users ...\n";
 
-  $user = new Sher_Core_Model_User();
-  $data = array();
-  $init_account = 18800000000;
-  for($i=0;$i<=500;$i++){
-    $account = $init_account + $i;
-    $city = '北京';
-    $email = (string)$account.'@139.com';
-    $data = array(
-      'account'  => (string)$account,
-      'password' => sha1('123456'),
-      'nickname' => (string)$account,
-      'state' => Sher_Core_Model_User::STATE_OK,
-      'role_id'  => Sher_Core_Model_User::ROLE_USER,
-      'quality' => 1,
-      'sex' => 1,
-      'city' => $city,
-      'email' => $email,
-      'kind' => 9,
-    );
-    $profile = $user->get_profile();
-    $profile['phone'] = (string)$account;
-    $profile['job'] = '设计师';
-    $data['profile'] = $profile;
+    $user = new Sher_Core_Model_User();
+    $data = array();
+    $account_prefix = array(138,168,188,189,186,158);
+    $cities = array('北京','深圳','广州','上海','杭州','南京','苏州','其他');
+    $step = 1;
+    $end = $start + 500;
+    for($i=$start; $i<=$end; $i+=$step){
+        $account = (int)$account_prefix[array_rand($account_prefix, 1)].'00000000' + $i;
+        $city = $cities[array_rand($cities, 1)];
+        $email = (string)$account.'@139.com';
+        $data = array(
+            'account'  => (string)$account,
+            'password' => sha1('thn456321'),
+            'nickname' => (string)$account,
+            'state'    => Sher_Core_Model_User::STATE_OK,
+            'role_id'  => Sher_Core_Model_User::ROLE_USER,
+            'quality'  => 1,
+            'sex'   => 1,
+            'city'  => $city,
+            'email' => $email,
+            'kind'  => 9,
+        );
+        $profile = $user->get_profile();
+        $profile['phone'] = (string)$account;
+        $profile['job'] = '设计师';
+        $data['profile'] = $profile;
 
-    try{
-      $ok = $user->create($data);
-      
-      if ($ok){
-        echo "Create the user[$account] is ok!...num: $i...\n";
-      }else{
-        echo "Create the user[$account] is fail!...num: $i...\n";
-      }
-      $new_data = $user->get_data();
-
-    }catch(Sher_Core_Model_Exception $e){
-      echo "Create the user[$account] failed: ".$e->getMessage();
+        try{
+            $ok = $user->create($data);
+            
+            if($ok){
+                echo "Create the user[$account] is ok!...num: $i...\n";
+            }else{
+                echo "Create the user[$account] is fail!...num: $i...\n";
+            }
+            
+        }catch(Sher_Core_Model_Exception $e){
+            echo "Create the user[$account] failed: ".$e->getMessage();
+        }
+        
+        $step = rand(1,8);
+        
+        sleep($step);
     }
-
-  }
 }
-batch_create_user();
 
-?>
+$start = 1000;
+batch_create_user($start);
