187b757ddb12fd955056abec11888f295899ab18
TianShuai <tianshuaiorc@sina.com>
2015-05-19 17:28:47 +0800

    avator v

---
+++ bin/fixed_topic_one.php
@@ -0,0 +1,33 @@
+#!/usr/bin/env php
+<?php
+/**
+ * 合并话题分类
+ */
+$config_file =  dirname(__FILE__).'/../deploy/app_config.php';
+if (!file_exists($config_file)) {
+    die("Can't find config_file: $config_file\n");
+}
+include $config_file;
+
+define('DOGGY_VERSION', $cfg_doggy_version);
+define('DOGGY_APP_ROOT', $cfg_app_deploy_root);
+define('DOGGY_APP_CLASS_PATH', $cfg_app_class_path);
+require $cfg_doggy_bootstrap;
+require $cfg_app_rc;
+
+set_time_limit(0);
+ini_set('memory_limit','512M');
+
+echo "Prepare to merge topic category...\n";
+
+
+$topic = new Sher_Core_Model_Topic();
+
+$updated = array(
+    'category_id' => 15,
+    'fid' => 11,
+);
+$ok = $topic->update_set(103215, $updated);
+
+echo "All category remath ok.\n";
+
