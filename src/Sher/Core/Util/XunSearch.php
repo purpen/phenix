1bfda1f1550a8d3cb6e26c8e355776fd9ac8f4d8
TianShuai <tianshuaiorc@sina.com>
2015-05-26 18:52:12 +0800

    xunsearch

--- src/Sher/Core/Util/XunSearch.php
+++ src/Sher/Core/Util/XunSearch.php
@@ -152,7 +152,7 @@ public function __construct() {
           array_push($x_tag_arr, sprintf("tags:%s", $v));
         }
         $x_tag_str = implode(' OR ', $x_tag_arr);
-        $str_f = sprintf('%s%s', $condition, $x_tag_str);
+        $str_f = sprintf('%s(%s)', $condition, $x_tag_str);
       }else{
         $search->addWeight('title', $str); // 增加附加条件：提升标题中包含 关键字 的记录的权重       
       }
