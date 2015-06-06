cd8d5b4d41543997ca252146bfcb29c35f266e36
lyf-THN <987083180@qq.com>
2015-06-06 14:54:09 +0800

    gopro

--- src/Sher/App/Action/Mall.php
+++ src/Sher/App/Action/Mall.php
@@ -26,11 +26,19 @@ class Sher_App_Action_Mall extends Sher_App_Action_Base {
 	}
 	
 	/**
-	 * lunar 祝眠灯
+	 * fitbit
 	 */
 	public function fitbit(){
 		$this->set_target_css_state('page_shop');
 		return $this->to_html_page('page/mall/fitbit.html');
+	}
+	
+	/**
+	 * GoPro
+	 */
+	public function gopro(){
+		$this->set_target_css_state('page_shop');
+		return $this->to_html_page('page/mall/gopro.html');
 	}
 
 
