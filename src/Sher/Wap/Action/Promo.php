adf8235d54aa0b58638491dd4e98bd53b83d9673
lyf-THN <987083180@qq.com>
2015-06-17 17:09:48 +0800

    six

--- src/Sher/Wap/Action/Promo.php
+++ src/Sher/Wap/Action/Promo.php
@@ -9,7 +9,7 @@ class Sher_Wap_Action_Promo extends Sher_Wap_Action_Base {
 	);
 	
 
-	protected $exclude_method_list = array('execute', 'test', 'coupon', 'dreamk', 'chinadesign', 'momo', 'watch', 'year_invite','year','jd','xin');
+	protected $exclude_method_list = array('execute', 'test', 'coupon', 'dreamk', 'chinadesign', 'momo', 'watch', 'year_invite','year','jd','xin','six');
 
 	
 	/**
@@ -20,7 +20,7 @@ class Sher_Wap_Action_Promo extends Sher_Wap_Action_Base {
 	}
 	
 	/**
-	 * 深圳蛋年专题
+	 * 69
 	 */
 	public function xin(){
 		
@@ -35,6 +35,24 @@ class Sher_Wap_Action_Promo extends Sher_Wap_Action_Base {
 	    $this->stash['wxSha1'] = sha1($wxOri);
 		
 		return $this->to_html_page('wap/promo/xin.html');
+	}
+	
+	/**
+	 * 69
+	 */
+	public function six(){
+		
+		$this->stash['page_title_suffix'] = '69';
+		//微信分享
+	    $this->stash['app_id'] = Doggy_Config::$vars['app.wechat.ser_app_id'];
+	    $timestamp = $this->stash['timestamp'] = time();
+	    $wxnonceStr = $this->stash['wxnonceStr'] = new MongoId();
+	    $wxticket = Sher_Core_Util_WechatJs::wx_get_jsapi_ticket();
+	    $url = $this->stash['current_url'] = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']; 
+	    $wxOri = sprintf("jsapi_ticket=%s&noncestr=%s&timestamp=%s&url=%s", $wxticket, $wxnonceStr, $timestamp, $url);
+	    $this->stash['wxSha1'] = sha1($wxOri);
+		
+		return $this->to_html_page('wap/promo/six.html');
 	}
 	
 	/**
