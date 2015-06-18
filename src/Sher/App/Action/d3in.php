dc48fe2fca8d1ffc4dd430919e268882f757541c
lyf-THN <987083180@qq.com>
2015-06-18 14:35:27 +0800

    d3in

---
+++ src/Sher/App/Action/d3in.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * D3IN铟立方未来实验室
+ * @author purpen
+ */
+class Sher_App_Action_D3in extends Sher_App_Action_Base {
+	public $stash = array(
+		'page'=>1,
+	);
+	
+	protected $exclude_method_list = array('execute', 'coupon', 'active','tool','member','yuyue','choose','ok');
+	
+	/**
+	 * 网站入口
+	 */
+	public function execute(){
+		return $this->to_html_page('page/d3in/index.html');
+	}
+	
+	/**
+	 * d3in
+	 */
+	public function d3in(){
+		return $this->to_html_page('page/d3in/index.html');
+	}
+	
+	/**
+	 * d3in 活动
+	 */
+	public function active(){
+		return $this->to_html_page('page/d3in/active.html');
+	}
+	
+	/**
+	 * d3in 活动
+	 */
+	public function tool(){
+		return $this->to_html_page('page/d3in/tool.html');
+	}
+	
+	/**
+	 * d3in 会员
+	 */
+	public function member(){
+		return $this->to_html_page('page/d3in/member.html');
+	}
+	
+	/**
+	 * d3in volunteer
+	 */
+	public function volunteer(){
+		return $this->to_html_page('page/d3in/volunteer.html');
+	}
+	
+	/**
+	 * d3in 预约
+	 */
+	public function yuyue(){
+		return $this->to_html_page('page/d3in/yuyue.html');
+	}
+	
+	/**
+	 * d3in 预约2
+	 */
+	public function choose(){
+		return $this->to_html_page('page/d3in/choose.html');
+	}
+	
+	/**
+	 * d3in 预约成功
+	 */
+	public function ok(){
+		return $this->to_html_page('page/d3in/ok.html');
+	}
+	
+}
+?>
