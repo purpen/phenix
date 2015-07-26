822c806ea0cc66c17ad7d560b339cd43a56272ff
TianShuai <tianshuaiorc@sina.com>
2015-07-26 18:50:35 +0800

    n

--- src/Sher/App/Action/Shop.php
+++ src/Sher/App/Action/Shop.php
@@ -10,7 +10,7 @@ class Sher_App_Action_Shop extends Sher_App_Action_Base implements DoggyX_Action
 		'sku' => '',
 		'type' => 0,
 		'category_id' => 0,
-		'sort' => 1,
+		'sort' => 0,
 		'topic_id' => '',
 		'page' => 1,
 		'size' => 3,
