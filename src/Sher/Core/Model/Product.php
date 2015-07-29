db13c3c4b08e9db7036ddb2d6269912c6ca01e68
TianShuai <tianshuaiorc@sina.com>
2015-07-29 18:10:54 +0800

    merge dev

--- src/Sher/Core/Model/Product.php
+++ src/Sher/Core/Model/Product.php
@@ -223,6 +223,8 @@ class Sher_Core_Model_Product extends Sher_Core_Model_Base {
 		'process_presaled' => 0,
 		# 是否在商店列表显示
 		'process_saled' => 0,
+    # 产品灵感
+    'process_idea' => 0,
 		
 		# 投票申请/产品灵感是否审核
 		'approved' => 0,
@@ -302,10 +304,10 @@ class Sher_Core_Model_Product extends Sher_Core_Model_Base {
             }else if ($row['stage'] == self::STAGE_IDEA){
                 $row['stage_label'] = '产品灵感';
             }else{
-                $row['stage_label'] = '未设置'; // 未知
+                $row['stage_label'] = '未设置1'; // 未知
             }
         }else{
-            $row['stage_label'] = '未设置'; // 未知  
+            $row['stage_label'] = '未设置2'; // 未知  
         }
 		
 		// HTML 实体转换为字符
