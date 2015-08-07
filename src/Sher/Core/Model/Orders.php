fb480f868cb33698a87bea24f11df086285d08ad
TianShuai <tianshuaiorc@sina.com>
2015-08-07 16:08:02 +0800

    order pay

--- src/Sher/Core/Model/Orders.php
+++ src/Sher/Core/Model/Orders.php
@@ -174,7 +174,7 @@ class Sher_Core_Model_Orders extends Sher_Core_Model_Base {
 		}
 
     // 支付方式
-    $row['trade_site_name'] = '--';
+    $row['trade_site_name'] = null;
     if (in_array($row['status'], array(10, 12, 13, 15, 20))){
       if (isset($row['trade_site']) && !empty($row['trade_site'])){
         $row['trade_site_name'] = $this->get_trade_site_label($row['trade_site']);
