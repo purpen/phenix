462df04999c4ef8aafea4f56c1734111a70be842
lyf-THN <987083180@qq.com>
2015-08-15 17:01:25 +0800

    custom

--- src/Sher/Wap/Action/Contest.php
+++ src/Sher/Wap/Action/Contest.php
@@ -11,7 +11,7 @@ class Sher_Wap_Action_Contest extends Sher_Wap_Action_Base {
 		'category_id' => 0,
 	);
 	
-	protected $exclude_method_list = array('execute','dream', 'dream2', 'topic', 'allist', 'allist2', 'get_list', 'show', 'rank', 'ajax_fetch_top_province', 'ajax_fetch_top_college', 'ajax_load_colleges','matcht','custom','about3','tooth');
+	protected $exclude_method_list = array('execute','dream', 'dream2', 'topic', 'allist', 'allist2', 'get_list', 'show', 'rank', 'ajax_fetch_top_province', 'ajax_fetch_top_college', 'ajax_load_colleges','matcht','custom','about3','tooth','power');
 	
 	/**
 	 * 社区入口
@@ -251,5 +251,8 @@ class Sher_Wap_Action_Contest extends Sher_Wap_Action_Base {
 		return $this->to_html_page('wap/contest/tooth.html');
 	}
 	
+	public function power(){
+		return $this->to_html_page('wap/contest/power.html');
+	}
 }
 
