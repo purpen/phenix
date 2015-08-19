7f7fe612e4f98f2a0291aa3e68e8fb4596ada24e
TianShuai <tianshuaiorc@sina.com>
2015-08-19 20:54:07 +0800

    contest link

--- src/Sher/Wap/Action/Contest.php
+++ src/Sher/Wap/Action/Contest.php
@@ -246,13 +246,36 @@ class Sher_Wap_Action_Contest extends Sher_Wap_Action_Base {
 	public function about3(){
 		return $this->to_html_page('wap/contest/about3.html');
 	}
-	
-	public function tooth(){
-		return $this->to_html_page('wap/contest/tooth.html');
-	}
-	
-	public function power(){
-		return $this->to_html_page('wap/contest/power.html');
-	}
+
+  /**
+   * 大赛详情
+   */
+  public function show(){
+    $id = $this->stash['id'];
+
+    $redirect_url = sprintf("%s/contest/about3", Doggy_Config::$vars['app.url.wap']);
+
+		if(empty($id)){
+			return $this->show_message_page('缺少请求参数！', $redirect_url);
+		}
+    
+    $model = new Sher_Core_Model_Contest();
+    $contest = $model->extend_load((int)$id);
+
+		if(empty($contest)){
+			return $this->show_message_page('访问的主题不存在！', $redirect_url);
+		}
+
+		// 增加pv++
+		$model->increase_counter('view_count', 1, $contest['_id']);
+        
+    $this->stash['contest'] = $contest;
+
+    $render = sprintf("wap/match/%s.html", $contest['short_name']);
+        
+    return $this->to_html_page($render);
+  }
+
+
 }
 
