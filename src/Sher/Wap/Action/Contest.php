4fa14ad2a021d3969f6a3000a3d6365470dd0974
TianShuai <tianshuaiorc@sina.com>
2015-08-19 21:00:44 +0800

    contest

--- src/Sher/Wap/Action/Contest.php
+++ src/Sher/Wap/Action/Contest.php
@@ -11,7 +11,7 @@ class Sher_Wap_Action_Contest extends Sher_Wap_Action_Base {
 		'category_id' => 0,
 	);
 	
-	protected $exclude_method_list = array('execute','dream', 'dream2', 'topic', 'allist', 'allist2', 'get_list', 'show', 'rank', 'ajax_fetch_top_province', 'ajax_fetch_top_college', 'ajax_load_colleges','matcht','custom','about3','tooth','power');
+	protected $exclude_method_list = array('execute','dream', 'dream2', 'topic', 'allist', 'allist2', 'get_list', 'show', 'rank', 'ajax_fetch_top_province', 'ajax_fetch_top_college', 'ajax_load_colleges','matcht','custom','about3', 'show');
 	
 	/**
 	 * 社区入口
