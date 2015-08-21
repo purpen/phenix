ad331aee74d28ae63110d952e7f0490634e29579
caowei <862302451@qq.com>
2015-08-21 16:26:37 +0800

    egou

---
+++ src/Sher/Admin/Action/Egou.php
@@ -0,0 +1,30 @@
+<?php
+    /**
+     * 后台易购管理
+     * @ author caowei@taihuoniao.com
+     */
+    class Sher_Admin_Action_Egou extends Sher_Admin_Action_Base {
+        
+        public $stash = array(
+            'id' => 0,
+            'page' => 1,
+            'size' => 20,
+        );
+        
+        public function execute(){
+            return $this->get_list();
+        }
+        
+        /**
+         * 投票列表
+         * @return string
+         */
+        public function get_list() {
+            $stage = isset($this->stash['stage']) ? $this->stash['stage'] : 0;
+            $this->set_target_css_state('page_egou');
+            $pager_url = Doggy_Config::$vars['app.url.admin'].'/egou?page=#p#';
+            $this->stash['pager_url'] = sprintf($pager_url, $stage);
+            return $this->to_html_page('admin/egou/list.html');
+        }
+    }
+?>
