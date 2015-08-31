8c0adf14012525a31589ae8a676065c1f07a10fb
TianShuai <tianshuaiorc@sina.com>
2015-09-01 01:54:23 +0800

    guess

--- src/Sher/App/Action/Shop.php
+++ src/Sher/App/Action/Shop.php
@@ -431,14 +431,21 @@ class Sher_App_Action_Shop extends Sher_App_Action_Base implements DoggyX_Action
                     'target_id' => array('$ne' => (int)$current_id),
                 );
                 $sword = array_values(array_unique(preg_split('/[,，\s]+/u', $sword)));
-                $result = Sher_Core_Service_Search::instance()->search(implode('',$sword), 'full', $addition_criteria, $options);     
+                //$result = Sher_Core_Service_Search::instance()->search(implode('',$sword), 'full', $addition_criteria, $options);     
+                $result = array();
             }
 
         }
-		
-		$this->stash['results'] = $result;
+
+    if(empty($result)){
+      return false;
+    }
+
+    $data = array();
+    $data['state'] = 1;
+    $data['results'] = $result;
         
-        return $this->ajax_json('', false, '', $this->stash);
+        return $this->ajax_json('', false, '', $data);
 	}
 	
 	/**
