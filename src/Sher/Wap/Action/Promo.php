8c30181fc4365a8831d04cbd9b3afd5ad3d853cf
lyf-THN <987083180@qq.com>
2015-09-14 14:46:30 +0800

    hy

--- src/Sher/Wap/Action/Promo.php
+++ src/Sher/Wap/Action/Promo.php
@@ -107,13 +107,13 @@ class Sher_Wap_Action_Promo extends Sher_Wap_Action_Base {
 	public function hy(){
 		$this->stash['page_title_suffix'] = '火眼';
 		//微信分享
-	    /*$this->stash['app_id'] = Doggy_Config::$vars['app.wechat.ser_app_id'];
+	    $this->stash['app_id'] = Doggy_Config::$vars['app.wechat.ser_app_id'];
 	    $timestamp = $this->stash['timestamp'] = time();
 	    $wxnonceStr = $this->stash['wxnonceStr'] = new MongoId();
 	    $wxticket = Sher_Core_Util_WechatJs::wx_get_jsapi_ticket();
 	    $url = $this->stash['current_url'] = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']; 
 	    $wxOri = sprintf("jsapi_ticket=%s&noncestr=%s&timestamp=%s&url=%s", $wxticket, $wxnonceStr, $timestamp, $url);
-	    $this->stash['wxSha1'] = sha1($wxOri);*/
+	    $this->stash['wxSha1'] = sha1($wxOri);
 		return $this->to_html_page('wap/promo/hy.html');
 	}
 	
