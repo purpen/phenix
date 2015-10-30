0708cba0f7d6b26e9722b5786c2727c2ddf4fb83
lyf <987083180@qq.com>
2015-10-30 11:01:11 +0800

    incubator

--- src/Sher/App/Action/Test.php
+++ src/Sher/App/Action/Test.php
@@ -8,7 +8,7 @@ class Sher_App_Action_Test extends Sher_App_Action_Base {
 		
 	);
 	
-	protected $exclude_method_list = array('execute','flat','add_user','test_func');
+	protected $exclude_method_list = array('execute','flat','add_user','test_func','tweleve');
 
 	/**
 	 * 默认入口
@@ -138,7 +138,7 @@ class Sher_App_Action_Test extends Sher_App_Action_Base {
 		return $this->to_html_page('wap/noodles.html');
 	}
 	public function tweleve() {
-		return $this->to_html_page('wap/auth/landing.html');
+		return $this->to_html_page('wap/incubator/view.html');
 	}
 
   /**
