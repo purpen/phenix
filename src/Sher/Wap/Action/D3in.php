4601b7edba96eb56767f3c7623ccfed2c29ef44f
lyf-THN <987083180@qq.com>
2015-11-03 18:07:52 +0800

    d3in_wap

---
+++ src/Sher/Wap/Action/D3in.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * D3IN铟立方未来实验室
+ * @author purpen
+ */
+class Sher_Wap_Action_D3in extends Sher_Wap_Action_Base {
+
+	public $stash = array(
+		'page'=>1,
+	    'size'=>50,
+	);
+	
+	protected $exclude_method_list = array('execute','d3in');
+	
+	/**
+	 * 网站入口
+	 */
+	public function execute(){
+		return $this->d3in();
+	}
+	
+	/**
+	 * d3in
+	 */
+	public function d3in(){
+		return $this->to_html_page('wap/d3in.html');
+	}
+	
+}
