960b2888eabed72cffbbbe96bd80b23ffd41ad7f
lyf-THN <987083180@qq.com>
2015-11-07 15:22:09 +0800

    zn

--- src/Sher/Wap/Action/Promo.php
+++ src/Sher/Wap/Action/Promo.php
@@ -24,16 +24,16 @@ class Sher_Wap_Action_Promo extends Sher_Wap_Action_Base {
 	 * 造逆 
 	 */
 	public function jdzn(){
-	/*	$this->stash['page_title_suffix'] = '绝密行动 代号“火眼”';
-		//微信分享
+			$this->stash['page_title_suffix'] = ' ';
+			//微信分享
 	    $this->stash['app_id'] = Doggy_Config::$vars['app.wechat.app_id'];
 	    $timestamp = $this->stash['timestamp'] = time();
 	    $wxnonceStr = $this->stash['wxnonceStr'] = new MongoId();
 	    $wxticket = Sher_Core_Util_WechatJs::wx_get_jsapi_ticket();
 	    $url = $this->stash['current_url'] = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']; 
 	    $wxOri = sprintf("jsapi_ticket=%s&noncestr=%s&timestamp=%s&url=%s", $wxticket, $wxnonceStr, $timestamp, $url);
-	    $this->stash['wxSha1'] = sha1($wxOri);*/
-		return $this->to_html_page('wap/promo/jdzn.html');
+	    $this->stash['wxSha1'] = sha1($wxOri);
+			return $this->to_html_page('wap/promo/jdzn.html');
 	}
 	
 	
