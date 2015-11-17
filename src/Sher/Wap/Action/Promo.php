d42f66f9ff3ad8a4eaf77cb821f3b77a30fa2374
tianshuai <tianshuaiorc@sina.com>
2015-11-17 21:41:36 +0800

    promo

--- src/Sher/Wap/Action/Promo.php
+++ src/Sher/Wap/Action/Promo.php
@@ -30,6 +30,18 @@ class Sher_Wap_Action_Promo extends Sher_Wap_Action_Base {
 	 * 创造 
 	 */
 	public function coin(){
+      // 记录浏览数
+      $num_mode = new Sher_Core_Model_SumRecord();
+      $num_mode->add_record('12', 'view_count', 4, 4); 
+
+			//微信分享
+	    $this->stash['app_id'] = Doggy_Config::$vars['app.wechat.app_id'];
+	    $timestamp = $this->stash['timestamp'] = time();
+	    $wxnonceStr = $this->stash['wxnonceStr'] = new MongoId();
+	    $wxticket = Sher_Core_Util_WechatJs::wx_get_jsapi_ticket();
+	    $url = $this->stash['current_url'] = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']; 
+	    $wxOri = sprintf("jsapi_ticket=%s&noncestr=%s&timestamp=%s&url=%s", $wxticket, $wxnonceStr, $timestamp, $url);
+	    $this->stash['wxSha1'] = sha1($wxOri);
 		
 		return $this->to_html_page('wap/promo/coin.html');
 	}
