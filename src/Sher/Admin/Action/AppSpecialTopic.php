18753e91bf45e82746f7b32d145c240fe7bd4fca
taihuoniao <862302451@qq.com>
2015-11-26 20:07:25 +0800

    add app_special_topic

---
+++ src/Sher/Admin/Action/AppSpecialTopic.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * 后台app专题管理
+ * @author caowei@taihuoniao.com
+ */
+class Sher_Admin_Action_AppSpecialTopic extends Sher_Admin_Action_Base implements DoggyX_Action_Initialize {
+	
+	public $stash = array(
+		'page' => 1,
+		'size' => 20,
+	);
+	
+	public function _init() {
+		$this->set_target_css_state('page_app_special_topic');
+    }
+	
+	/**
+	 * 入口
+	 */
+	public function execute() {
+		return $this->get_list();
+	}
+	
+	/**
+	 * 列表
+	 */
+	public function get_list() {
+
+		$page = (int)$this->stash['page'];
+		$this->stash['user_id'] = $this->visitor->id;
+		
+		//清空私信提醒数量
+		if($this->visitor->counter['message_count']>0){
+		  $this->visitor->update_counter($this->visitor->id, 'message_count');   
+		}
+		$this->stash['pager_url'] = Doggy_Config::$vars['app.url.admin'].'/private_letter/get_list?page=#p#';
+		
+		return $this->to_html_page('admin/app_special_topic/list.html');
+	}
+	
+	/**
+	 * 添加页面
+	 */
+	public function add_page(){
+		return $this->to_html_page('admin/app_special_topic/save.html');
+	}
+}
+
