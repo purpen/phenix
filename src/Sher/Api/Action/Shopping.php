331c37eff3fbe814691e2dfbd6b23f4d6d252d2c
tianshuai <tianshuaiorc@sina.com>
2016-01-29 14:03:37 +0800

    shopping

--- src/Sher/Api/Action/Shopping.php
+++ src/Sher/Api/Action/Shopping.php
@@ -222,7 +222,7 @@ class Sher_Api_Action_Shopping extends Sher_Api_Action_Base{
 
       // 加载可用红包
       $bonus_service = Sher_Core_Service_Bonus::instance();
-      $bonus_result = $bonus_service->get_all_list(array('used'=>1, 'expired_at'=>array('$gt'=>time())), array('page'=>1, 'size'=>20));
+      $bonus_result = $bonus_service->get_all_list(array('user_id'=>$user_id, 'used'=>1, 'expired_at'=>array('$gt'=>time())), array('page'=>1, 'size'=>20));
       $usable_bonus = !empty($bonus_result['rows']) ? $bonus_result['rows'] : array();
 
       // 删除购物车
@@ -338,7 +338,7 @@ class Sher_Api_Action_Shopping extends Sher_Api_Action_Base{
 
     // 加载可用红包
     $bonus_service = Sher_Core_Service_Bonus::instance();
-    $bonus_result = $bonus_service->get_all_list(array('used'=>1, 'expired_at'=>array('$gt'=>time())), array('page'=>1, 'size'=>20));
+    $bonus_result = $bonus_service->get_all_list(array('user_id'=>$user_id, 'used'=>1, 'expired_at'=>array('$gt'=>time())), array('page'=>1, 'size'=>20));
     $usable_bonus = !empty($bonus_result['rows']) ? $bonus_result['rows'] : array();
 		
 		// 获取快递费用
