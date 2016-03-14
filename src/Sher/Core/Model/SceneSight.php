aeb448d672e4d8a1c33d8256cd3b471e9f106520
taihuoniao <862302451@qq.com>
2016-03-14 15:00:07 +0800

    edit

--- src/Sher/Core/Model/SceneSight.php
+++ src/Sher/Core/Model/SceneSight.php
@@ -10,13 +10,48 @@ class Sher_Core_Model_SceneSight extends Sher_Core_Model_Base {
     protected $schema = array(
 		# 标题
 		'title' => '',
+		# 创建者
+		'user_id' => 0,
+		# 描述
+		'des' => '',
         # 分类
         'type' => '',
-		# 图片地址
-		'images' => '',
-        # 使用次数
+		
+		# 所属情景
+		'scene_id' => 0,
+		# 标签
+		'tags' => array(),
+		# 产品
+		'product' => array(),
+		# 地理位置
+		 'location'  => array(
+            'type' => 'Point',
+            # 经度,纬度
+            'coordinates' => array(
+				'longitude' => 0,
+				'latitude' => 0
+			),
+        ),
+		# 地址
+        'address' => '',
+		
+        # 封面
+		'cover_id' => '',
+		
+		# 使用次数
         'used_count' => 0,
-        # 是否启用
+		# 浏览数
+    	'view_count' => 0,
+		# 喜欢数
+        'love_count' => 0,
+		# 评论数 
+    	'comment_count' => 0,
+		
+		# 精选
+		'fine'  => 0,
+		# 审核
+		'is_check' => 1,
+		# 是否启用
 		'status' => 1,
     );
 	
