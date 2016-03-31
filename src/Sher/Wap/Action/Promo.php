cc18235ab38bedda91c9268c45b931ba8ac96148
tianshuai <tianshuaiorc@sina.com>
2016-03-31 20:37:19 +0800

    promo

--- src/Sher/Wap/Action/Promo.php
+++ src/Sher/Wap/Action/Promo.php
@@ -24,7 +24,7 @@ class Sher_Wap_Action_Promo extends Sher_Wap_Action_Base {
      */
     public function android_download(){
       $type = isset($this->stash['type']) ? (int)$this->stash['type'] : 1;
-      $url = 'http://frstatic.qiniudn.com/download%2Fapp-release_001.apk';
+      $url = 'http://frstatic.qiniudn.com/download%2Fapp-release_002.apk';
   		return $this->to_redirect($url);
     }
 	
