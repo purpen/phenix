b8a0ac7a23cc6e99ecd9de3413809239cac46a0a
tianshuai <tianshuaiorc@sina.com>
2016-04-12 23:45:55 +0800

    wap subject

--- src/Sher/Wap/Action/SpecialSubject.php
+++ src/Sher/Wap/Action/SpecialSubject.php
@@ -57,9 +57,19 @@ class Sher_Wap_Action_SpecialSubject extends Sher_Wap_Action_Base {
 		
 		if($special_subject['kind']==Sher_Core_Model_SpecialSubject::KIND_APPOINT){
 			if(!empty($special_subject['product_ids'])){
+
+
 			  $product_model = new Sher_Core_Model_Product();
 			  foreach($special_subject['product_ids'] as $k=>$v){
           $product = $product_model->extend_load((int)$v);
+
+          // 封面图url
+          $asset_query = array('parent_id'=>$product['_id'], 'asset_type'=>11);
+          $asset_options['page'] = 1;
+          $asset_options['size'] = 1;
+          $asset_service = Sher_Core_Service_Asset::instance();
+          $asset_result = $asset_service->get_asset_list($asset_query, $asset_options);
+          $product['cover_url'] = !empty($asset_result['rows']) ? $asset_result['rows'][0]['thumbnails']['aub']['view_url'] : null;
           if(!empty($product)){
             array_push($product_arr, $product);
           }
@@ -67,6 +77,7 @@ class Sher_Wap_Action_SpecialSubject extends Sher_Wap_Action_Base {
 			} // endif empty
 		} // endif kind
 		
+    $this->stash['special_subject'] = $special_subject;
 		if($special_subject['kind']==Sher_Core_Model_SpecialSubject::KIND_CUSTOM){  // 用户自定义输出
       $this->stash['content'] = $special_subject['content'];
 			$tpl = 'wap/special_subject/custom_view.html';
