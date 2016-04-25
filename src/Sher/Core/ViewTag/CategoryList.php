88ef841bf83c281d7943e5e25adde12992902411
tianshuai <tianshuaiorc@sina.com>
2016-04-25 23:25:56 +0800

    web product index

--- src/Sher/Core/ViewTag/CategoryList.php
+++ src/Sher/Core/ViewTag/CategoryList.php
@@ -107,35 +107,41 @@ class Sher_Core_ViewTag_CategoryList extends Doggy_Dt_Tag {
 			
 	        for ($i=0; $i<count($categories); $i++) {
 			
-				if ($categories[$i]['domain'] == Sher_Core_Util_Constant::TYPE_TOPIC){
-					$categories[$i]['view_url'] = Sher_Core_Helper_Url::topic_list_url($categories[$i]['_id']);
-				} else if ($categories[$i]['domain'] == Sher_Core_Util_Constant::TYPE_PRODUCT){
-					switch($stage){
-						case Sher_Core_Model_Product::STAGE_VOTE:
-							$categories[$i]['view_url'] = Sher_Core_Helper_Url::vote_list_url($categories[$i]['_id']);
-							break;
-						case Sher_Core_Model_Product::STAGE_PRESALE:
-							$categories[$i]['view_url'] = Sher_Core_Helper_Url::sale_list_url($categories[$i]['_id']);
-							break;
-						case Sher_Core_Model_Product::STAGE_SHOP:
-							$categories[$i]['view_url'] = Sher_Core_Helper_Url::shop_list_url($categories[$i]['_id']);
-							break;
-					}
-				}
+            if ($categories[$i]['domain'] == Sher_Core_Util_Constant::TYPE_TOPIC){
+              $categories[$i]['view_url'] = Sher_Core_Helper_Url::topic_list_url($categories[$i]['_id']);
+            } else if ($categories[$i]['domain'] == Sher_Core_Util_Constant::TYPE_PRODUCT){
+              switch($stage){
+                case Sher_Core_Model_Product::STAGE_VOTE:
+                  $categories[$i]['view_url'] = Sher_Core_Helper_Url::vote_list_url($categories[$i]['_id']);
+                  break;
+                case Sher_Core_Model_Product::STAGE_PRESALE:
+                  $categories[$i]['view_url'] = Sher_Core_Helper_Url::sale_list_url($categories[$i]['_id']);
+                  break;
+                case Sher_Core_Model_Product::STAGE_SHOP:
+                  $categories[$i]['view_url'] = Sher_Core_Helper_Url::shop_list_url($categories[$i]['_id']);
+                  break;
+              }
+            }
         		
 	        	if(empty($current)){
 	        		continue;
 	        	}
-	            if (!is_array($current)){
-	            	if($current == $categories[$i]['_id']){
-	            		$categories[$i]['active'] = 'active';
-	            	}
-	            }else{
-	                if(in_array($categories[$i]['_id'], $current)){
-	                    $categories[$i]['active'] = 'active';
-	                }
-	            }
-	        }
+            if (!is_array($current)){
+              if($current == $categories[$i]['_id']){
+                $categories[$i]['active'] = 'active';
+              }
+            }else{
+                if(in_array($categories[$i]['_id'], $current)){
+                    $categories[$i]['active'] = 'active';
+                }
+            }
+
+            if($i%2==0){
+              $categories[$i]['odd'] = false;
+            }else{
+              $categories[$i]['odd'] = true;           
+            }
+	        } //end for
 			
 			// 重写rows
 			$result['rows'] = $categories;
