b96e87a0602febbbcc3246151d1819e2f355837e
tianshuai <tianshuaiorc@sina.com>
2016-04-30 05:10:28 +0800

    category

--- src/Sher/Api/Action/Category.php
+++ src/Sher/Api/Action/Category.php
@@ -5,7 +5,7 @@
  */
 class Sher_Api_Action_Category extends Sher_Api_Action_Base {
 	
-	protected $filter_user_method_list = array('execute', 'getlist');
+	protected $filter_user_method_list = array('execute', 'getlist', 'fetch_child_tags');
 	
 	/**
 	 * 入口
@@ -46,7 +46,7 @@ class Sher_Api_Action_Category extends Sher_Api_Action_Base {
         $service = Sher_Core_Service_Category::instance();
         $result = $service->get_category_list($query, $options);
 
-        $scene_tags_model = new Sher_Core_Model_SceneTags();
+        //$scene_tags_model = new Sher_Core_Model_SceneTags();
 
         // 过滤多余属性
         $filter_fields = array('view_url', 'state', 'is_open', '__extend__');
@@ -63,9 +63,10 @@ class Sher_Api_Action_Category extends Sher_Api_Action_Base {
             $data[$i]['app_cover_s_url'] = sprintf("%s-p325x200.jpg", $result['rows'][$i]['app_cover_url']);
           }
 
+          /**
           $scene_tags_arr = array();
 
-          // 加载标签
+          // 加载标签--暂不需要,加入新接口，先注掉
           if($data[$i]['domain']==10){
             if(isset($data[$i]['tag_id']) && !empty($data[$i]['tag_id'])){
 
@@ -87,6 +88,7 @@ class Sher_Api_Action_Category extends Sher_Api_Action_Base {
             }
           } // endif domain==10
           $data[$i]['scene_tags'] = $scene_tags_arr;
+          **/
 
         }
 
@@ -95,6 +97,31 @@ class Sher_Api_Action_Category extends Sher_Api_Action_Base {
 
 		return $this->api_json('请求成功', 0, $result);
 	}
+
+  /**
+   * 获取分类下的标签
+   */
+  public function fetch_child_tags(){
+    $tag_id = isset($this->stash['tag_id']) ? (int)$this->stash['tag_id'] : 0;
+
+    $scene_tags_arr = array();
+    $scene_tags_model = new Sher_Core_Model_SceneTags();
+    $tag_query = array(
+      'type' => 2,
+      'parent_id' => $tag_id,
+      'status' => Sher_Core_Model_SceneTags::STATE_OK,
+    );
+    $tag_options = array(
+      //'field' = array('_id'=>1);
+    );
+    $scene_tags = $scene_tags_model->find($tag_query, $tag_options);
+    if($scene_tags){
+      foreach($scene_tags as $k=>$v){
+        array_push($scene_tags_arr, array('_id'=>$v['_id'], 'title_cn'=>$v['title_cn']));
+      }
+    }
+    return $this->api_json('请求成功', 0, array('tags'=>$scene_tags_arr)); 
+  }
 
 	
 }
