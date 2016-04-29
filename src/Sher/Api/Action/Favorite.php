ce1f2004c54275bd589714d4a82f5afc18f38bcf
taihuoniao <862302451@qq.com>
2016-04-30 07:13:53 +0800

    edit user info

--- src/Sher/Api/Action/Favorite.php
+++ src/Sher/Api/Action/Favorite.php
@@ -123,7 +123,7 @@ class Sher_Api_Action_Favorite extends Sher_Api_Action_Base {
         $filter_fields  = array('tag_s','__extend__');
         $result['rows'] = Sher_Core_Helper_FilterFields::filter_fields($result['rows'], $filter_fields, 2);
 		
-		var_dump($result['rows']);die;
+		//var_dump($result['rows']);die;
 		return $this->api_json('请求成功', 0, $result);
 	}
 	
