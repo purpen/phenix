56fd933dd3c42759175b65a80f6429cd0fdf08ff
tianshuai <tianshuaiorc@sina.com>
2016-05-07 15:01:02 +0800

    add app_active record

--- src/Sher/AppAdmin/Action/Console.php
+++ src/Sher/AppAdmin/Action/Console.php
@@ -10,6 +10,10 @@ class Sher_AppAdmin_Action_Console extends Sher_AppAdmin_Action_Base {
 		'page' => 1,
 		'sort' => 'latest',
 		'rank' => 'day',
+    'uuid' => null,
+    'channel_id' => null,
+    'kind' => 1,
+    'device' => 1,
 	);
 	
 	/**
@@ -31,7 +35,41 @@ class Sher_AppAdmin_Action_Console extends Sher_AppAdmin_Action_Base {
 		// 判断左栏类型
 		$this->stash['show_type'] = "console";
 		
-        return $this->to_html_page('app_admin/dashboard.html');
+    return $this->to_html_page('app_admin/dashboard.html');
+  }
+
+  /**
+   * 激活量记录
+   */
+  public function user_active_record(){
+		// 判断左栏类型
+		$this->stash['show_type'] = "console";
+    $this->set_target_css_state('page_app_user_record');
+
+		$pager_url = Doggy_Config::$vars['app.url.app_admin'].'/console/user_active_record?uuid=%s&channel_id=%d&kind=%d&device=%d&page=#p#';
+
+		$this->stash['pager_url'] = sprintf($pager_url, $this->stash['uuid'], $this->stash['channel_id'], $this->stash['kind'], $this->stash['device']);
+
+    return $this->to_html_page('app_admin/user_active_record.html');
+
+  }
+
+  /**
+   * 激活量删除
+   */
+  public function app_user_record_deleted(){
+    $id = isset($this->stash['id']) ? $this->stash['id'] : null;
+		if(empty($id)){
+			return $this->ajax_note('请求参数为空', true);
+		}
+		$model = new Sher_Core_Model_AppUserRecord();
+		if($model->remove($id)){
+      $model->mock_after_remove($id);
+		}
+		
+		$this->stash['id'] = $id;
+		return $this->to_taconite_page('app_admin/del_ok.html');
+
   }
 	
 }
---
+++ src/Sher/Core/ViewTag/AppUserRecordList.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * app用户激活列表标签
+ * @author tianshuai
+ */
+class Sher_Core_ViewTag_AppUserRecordList extends Doggy_Dt_Tag {
+    protected $argstring;
+	
+    public function __construct($argstring, $parser, $pos = 0) {
+        $this->argstring = $argstring;
+    }
+
+    /**
+     * 列表的条件保持与索引顺序一致(non-PHPdoc)
+     * @see Doggy/Dt/Doggy_Dt_Node#render()
+     */
+    public function render($context, $stream) {
+        $page = 1;
+        $size = 10;
+
+		    $uuid = 0;
+		    $channel_id = 0;
+		    $kind = 0;
+		    $device = 0;
+
+		
+        $var = 'list';
+        $include_pager = 0;
+        $pager_var = 'pager';
+		
+		    $sort_field = 'latest';
+		
+        extract($this->resolve_args($context,$this->argstring,EXTR_IF_EXISTS));
+
+        $page = (int) $page;
+        $page || $page = 1;
+        $size = (int)$size;
+		
+        $query = array();
+
+		
+        if($uuid){
+          $query['uuid'] = $uuid;
+        }
+        if($channel_id){
+          $query['channel_id'] = (int)$channel_id;
+        }
+        if($kind){
+          $query['kind'] = (int)$kind;
+        }
+        if($device){
+          $query['device'] = (int)$device;
+        }
+
+        $service = Sher_Core_Service_AppUserRecord::instance();
+        $options['page'] = $page;
+        $options['size'] = $size;
+		$options['sort_field'] = $sort_field;
+        $result = $service->get_app_user_record_list($query,$options);
+		
+        $context->set($var, $result);
+		
+        if ($include_pager) {
+            $context->set($pager_var,$result['pager']);
+        }
+		
+    }
+}
+
