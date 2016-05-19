496c9f276242080e279298808ff64ba0de252d70
tianshuai <tianshuaiorc@sina.com>
2016-05-19 10:52:08 +0800

    shopping

--- src/Sher/Api/Action/Shopping.php
+++ src/Sher/Api/Action/Shopping.php
@@ -73,6 +73,8 @@ class Sher_Api_Action_Shopping extends Sher_Api_Action_Base{
     if(!isset($this->stash['array']) || empty($this->stash['array'])){
       return $this->api_json('购物车为空！', 3001); 
     }
+    // app来源
+    $app_type = isset($this->stash['app_type']) ? (int)$this->stash['app_type'] : 1;
     // 第一版不加此参数，购物车数量是多少就买多少
     $n = isset($this->stash['n']) ? (int)$this->stash['n'] : 1;
     $cart_arr = json_decode($this->stash['array']);
@@ -194,19 +196,22 @@ class Sher_Api_Action_Shopping extends Sher_Api_Action_Base{
 			// 优惠活动费用
 			$coin_money = 0.0;
 
-      // 用户是否首次下单立减 秒杀不参与
-      $user_model = new Sher_Core_Model_User();
-      $user = $user_model->load($this->current_user_id);
-      if(empty($user)){
-        return false;
-      }
-      if(isset($user['identify']['is_app_first_shop']) && $user['identify']['is_app_first_shop']==1){
-        //首次下单立减非首次下单用户过滤
-      }else{
-        if(empty($kind)){ // 其它活动不参与
-          $kind = 4;
-          $coin_money = Sher_Core_Util_Constant::APP_FIRST_COIN_MONEY;
-        }    
+
+      // 用户是否首次下单立减 秒杀、Fiu不参与
+      if($app_type==1){
+        $user_model = new Sher_Core_Model_User();
+        $user = $user_model->load($this->current_user_id);
+        if(empty($user)){
+          return false;
+        }
+        if(isset($user['identify']['is_app_first_shop']) && $user['identify']['is_app_first_shop']==1){
+          //首次下单立减非首次下单用户过滤
+        }else{
+          if(empty($kind)){ // 其它活动不参与
+            $kind = 4;
+            $coin_money = Sher_Core_Util_Constant::APP_FIRST_COIN_MONEY;
+          }    
+        }     
       }
 			
 			// 红包金额
@@ -275,6 +280,8 @@ class Sher_Api_Action_Shopping extends Sher_Api_Action_Base{
 		$target_id = isset($this->stash['target_id'])?(int)$this->stash['target_id']:0;
 		$type = isset($this->stash['type'])?(int)$this->stash['type']:0;
 		$quantity = isset($this->stash['n'])?(int)$this->stash['n']:1;
+    // app来源
+    $app_type = isset($this->stash['app_type']) ? (int)$this->stash['app_type'] : 1;
     $result = array();
     $is_app_snatched = false;
     $usable_bonus = array();
@@ -372,7 +379,7 @@ class Sher_Api_Action_Shopping extends Sher_Api_Action_Base{
 		$total_money = (float)$price*$quantity;
 		$items_count = 1;
 
-		$order_info = $this->create_temp_order($items, $total_money, $items_count, $kind);
+		$order_info = $this->create_temp_order($items, $total_money, $items_count, $kind, $app_type);
 		if (empty($order_info)){
       return $this->api_json('系统出了小差，请稍后重试！', 3006);
     }
@@ -1258,7 +1265,7 @@ class Sher_Api_Action_Shopping extends Sher_Api_Action_Base{
 	/**
 	 * 生产临时订单
 	 */
-	protected function create_temp_order($items=array(),$total_money,$items_count,$kind=0){
+	protected function create_temp_order($items=array(),$total_money,$items_count,$kind=0, $app_type=1){
 		$data = array();
 		$data['items'] = $items;
 		$data['total_money'] = $total_money;
@@ -1276,19 +1283,21 @@ class Sher_Api_Action_Shopping extends Sher_Api_Action_Base{
 		// 优惠活动费用
 		$coin_money = 0.0;
 
-    // 用户是否首次下单立减 秒杀不参与
-    $user_model = new Sher_Core_Model_User();
-    $user = $user_model->load($this->current_user_id);
-    if(empty($user)){
-      return false;
-    }
-    if(isset($user['identify']['is_app_first_shop']) && $user['identify']['is_app_first_shop']==1){
-      //首次下单立减非首次下单用户过滤
-    }else{
-      if(empty($kind)){ // 秒杀不参与
-        $kind = 4;
-        $coin_money = Sher_Core_Util_Constant::APP_FIRST_COIN_MONEY;
-      }    
+    // 用户是否首次下单立减 秒杀、Fiu不参与
+    if($app_type==1){
+      $user_model = new Sher_Core_Model_User();
+      $user = $user_model->load($this->current_user_id);
+      if(empty($user)){
+        return false;
+      }
+      if(isset($user['identify']['is_app_first_shop']) && $user['identify']['is_app_first_shop']==1){
+        //首次下单立减非首次下单用户过滤
+      }else{
+        if(empty($kind)){ // 秒杀不参与
+          $kind = 4;
+          $coin_money = Sher_Core_Util_Constant::APP_FIRST_COIN_MONEY;
+        }    
+      }   
     }
 		
 		// 红包金额
