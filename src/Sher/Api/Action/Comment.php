76f7f47d55a7e80d3f86ea3608f78f118f93bed3
tianshuai <tianshuaiorc@sina.com>
2016-05-23 23:49:47 +0800

    sceneproduct

--- src/Sher/Api/Action/Comment.php
+++ src/Sher/Api/Action/Comment.php
@@ -44,8 +44,7 @@ class Sher_Api_Action_Comment extends Sher_Api_Action_Base {
 		$options['some_fields'] = array(
 		  '_id'=>1, 'user_id'=>1, 'content'=>1, 'star'=>1, 'target_id'=>1, 'target_user_id'=>1, 'sku_id'=>1,
 		  'deleted'=>1, 'reply_user_id'=>1, 'floor'=>1, 'type'=>1, 'sub_type'=>1, 'user'=>1, 'target_user'=>1,
-		  'love_count'=>1, 'invented_love_count'=>1, 'is_reply'=>1, 'reply_id'=>1, 'created_on'=>1, 'updated_on'=>1,
-		  'created_at'=>1, 'reply_comment'=>1,
+		  'love_count'=>1, 'invented_love_count'=>1, 'is_reply'=>1, 'reply_id'=>1, 'created_on'=>1, 'updated_on'=>1, 'reply_comment'=>1,
 		);
 		
 		// 查询条件
@@ -113,6 +112,7 @@ class Sher_Api_Action_Comment extends Sher_Api_Action_Base {
       }else{
         $data[$i]['reply_comment'] = null;
       }
+      $data[$i]['created_at'] = Sher_Core_Helper_Util::relative_datetime($data[$i]['created_on']);
 
 		}
 		$result['rows'] = $data;
