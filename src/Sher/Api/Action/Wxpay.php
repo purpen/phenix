eb0e8fad1ab29def5c3683588b65269cd2e21916
tianshuai <tianshuaiorc@sina.com>
2016-05-30 00:13:52 +0800

    wx notify

--- src/Sher/Api/Action/Wxpay.php
+++ src/Sher/Api/Action/Wxpay.php
@@ -413,7 +413,7 @@
       require_once 'wxpay-sdk/lib/WxPay.Notify.php';
 			
 			// 返回微信支付结果通知信息
-      $notify = new WxPayNotify();
+      $notify = new PayNotifyCallBack();
 			$result = $notify->Handle();
 			if(!$result){
         Doggy_Log_Helper::warn("app微信获取异步获取通知失败~fiu!");
