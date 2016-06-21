40836d1e873d59f76a4130cadb34696ec4a09366
tianshuai <tianshuaiorc@sina.com>
2016-06-21 18:40:46 +0800

    product

--- src/Sher/Api/Action/Product.php
+++ src/Sher/Api/Action/Product.php
@@ -114,6 +114,7 @@ class Sher_Api_Action_Product extends Sher_Api_Action_Base {
 		
 		// 请求参数
 		$category_id = isset($this->stash['category_id']) ? (int)$this->stash['category_id'] : 0;
+		$category_tags = isset($this->stash['category_tags']) ? $this->stash['category_tags'] : null;
 		$user_id  = isset($this->stash['user_id']) ? (int)$this->stash['user_id'] : 0;
 		$stick = isset($this->stash['stick']) ? (int)$this->stash['stick'] : 0;
 		$sort = isset($this->stash['sort']) ? (int)$this->stash['sort'] : 0;
@@ -191,6 +192,9 @@ class Sher_Api_Action_Product extends Sher_Api_Action_Base {
 			foreach($some_fields as $key=>$value){
 				$data[$i][$key] = isset($result['rows'][$i][$key])?$result['rows'][$i][$key]:0;
 			}
+            if($data[$i]['category_tags']==0){
+                $data[$i]['category_tags'] = array();
+            }
 			// 封面图url
 			$data[$i]['cover_url'] = $result['rows'][$i]['cover']['thumbnails']['apc']['view_url'];
 			// 用户信息
