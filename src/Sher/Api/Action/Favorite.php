3f3c1dd108aa1fd91845fcb0f2fbb476b952c9b0
tianshuai <tianshuaiorc@sina.com>
2016-08-04 19:02:20 +0800

    favorite

--- src/Sher/Api/Action/Favorite.php
+++ src/Sher/Api/Action/Favorite.php
@@ -360,10 +360,7 @@ class Sher_Api_Action_Favorite extends Sher_Api_Action_Base {
                     $product = null;
                     $result['rows'][$k]['product'] = $product;
                     break;
-                case 1:
-                    $product = null;
-                    $result['rows'][$k]['product'] = $product;
-                    break;
+
                 default:
             }
 
