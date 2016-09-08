20f8d807c622213e09567a5296187a4fd84e340a
tianshuai <tianshuaiorc@sina.com>
2016-09-08 17:51:16 +0800

    bin check topic

--- bin/check_topic.php
+++ bin/check_topic.php
@@ -23,36 +23,32 @@ ini_set('memory_limit','512M');
 
 echo "set topic add inlink-tag ...\n";
 
-$model = new Sher_Core_Model_Topic();
+$topic_model = new Sher_Core_Model_Topic();
 $asset_model = new Sher_Core_Model_Asset();
 $page = 1;
 $size = 200;
 $is_end = false;
 $total = 0;
 while(!$is_end){
-  $time = time() - 15*24*60*60;
 	$query = array();
-  $query['created_on'] = array('$gt'=>$time);
-	$options = array('field' => array('_id', 'asset_count'),'page'=>$page,'size'=>$size);
-	$list = $model->find($query, $options);
+    $query['attrbute'] = 3;
+	$options = array('field' => array('_id', 'attrbute'),'page'=>$page,'size'=>$size);
+	$list = $topic_model->find($query, $options);
 	if(empty($list)){
 		echo "get topic list is null,exit......\n";
 		break;
 	}
 	$max = count($list);
 	for ($i=0; $i < $max; $i++) {
-    $asset_count = $list[$i]['asset_count'];
-    $id = $list[$i]['_id'];
-    if(empty($asset_count)){
-      $new_asset_count = $asset_model->count(array('parent_id'=>$id, 'asset_type'=>55));
-      if(!empty($new_asset_count)){
-        //$model->update_set($id, array('asset_count'=>$new_asset_count));
-        echo "desc is empty id: $id. \n";
-        echo "new asset account $new_asset_count \n";
-        $total++;
-      }
-
-    }
+        $id = $list[$i]['_id'];
+        $attrbute = $list[$i]['attrbute'];
+        if($attrbute!=3) continue;
+        $ok = true;
+        //$ok = $topic_model->update_set($id, array('category_id'=>241, 'fid'=>237));
+        if($ok){
+            $total++;
+            echo "update ok topic id: $id.\n";
+        }
     
 	}
 	if($max < $size){
