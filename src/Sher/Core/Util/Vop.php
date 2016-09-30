ddae88c9eae3265a420ef0443031e722bfb8fcad
tianshuai <tianshuaiorc@sina.com>
2016-09-30 17:20:55 +0800

    vop

---
+++ src/Sher/Core/Util/Vop.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 京东开普勒计划
+ * @author tianshuai
+ */
+class Sher_Core_Util_Vop {
+
+    $url = "https://kploauth.jd.com/oauth/token?grant_type=password&app_key=d843f6822b1b4fb5a3d24413739de20c&app_secret=45e24907e71a46aab501024cadbd8245&state=0&username=kepler_test&password=e10adc3949ba59abbe56e057f20f883e"
+    
+
+    public function __construct() {
+
+    }
+	
+	/**
+	 * 获取token
+	 */
+    public static function fetchToken()
+    {
+        
+    
+	}
+
+	
+}
+
