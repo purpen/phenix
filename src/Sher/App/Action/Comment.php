da0503a84941d63c8bb5ddf352b4a60e2598030b
tianshuai <tianshuaiorc@sina.com>
2016-11-14 15:16:57 +0800

    product comment

--- src/Sher/App/Action/Comment.php
+++ src/Sher/App/Action/Comment.php
@@ -313,6 +313,64 @@ class Sher_App_Action_Comment extends Sher_App_Action_Base {
 	}
 
 	/**
+	 * 用户发表评价--批量
+	 */
+	public function batch_ajax_evaluate(){
+
+		$order_id = isset($this->stash['order_id'])?$this->stash['order_id']:null;
+
+        $target_id = isset($this->stash['target_id'][0])?$this->stash['target_id'][0]:null;
+
+return $this->ajax_json('操作成功!', false, null, array('target_id'=>$target_id));
+
+		
+		$row = array();
+		$row['user_id'] = $this->visitor->id;
+		$row['star'] = $this->stash['star'];
+		$row['target_id'] = $this->stash['target_id'];
+		$row['content'] = $this->stash['content'];
+		$row['type'] = (int)$this->stash['type'];
+		$row['sku_id'] = isset($this->stash['sku'])?(int)$this->stash['sku']:0;
+		$row['from_site'] = isset($this->stash['from_site'])?(int)$this->stash['from_site']:1;
+		
+		// 验证数据
+		if(empty($row['target_id']) || empty($row['content']) || empty($row['star'])){
+			return $this->ajax_note('获取数据错误,请重新提交', true);
+		}
+
+		$model = new Sher_Core_Model_Comment();
+
+		$query = array();
+		$query['target_id'] = $row['target_id'];
+		$query['type'] = $row['type'];
+		$query['user_id'] = $row['user_id'];
+		$query['sku_id'] = $row['sku_id'];
+    // 不验证重复性
+    //$has_one = $model->first($query);
+    $has_one = false;
+		if(!empty($has_one)){
+		  return $this->ajax_note('该商品不能重复评价!', true);
+		}
+
+		$ok = $model->apply_and_save($row);
+		if($ok){
+			$comment_id = $model->id;
+			$this->stash['comment'] = &$model->extend_load($comment_id);
+
+      // 更新订单状态为完成
+      if(!empty($order_id)){
+        $orders_model = new Sher_Core_Model_Orders();
+        $order = $orders_model->load((string)$order_id);
+        if(!empty($order) && $order['user_id']==$this->visitor->id && $order['status']==Sher_Core_Util_Constant::ORDER_EVALUATE){
+          $order_ok = $orders_model->finish_order($order_id, array('user_id'=>$order['user_id']));
+        }
+      }
+		} // if ok
+		
+        return $this->ajax_json('操作成功!', false);
+	}
+
+	/**
 	 * 点赞
 	 */
 	public function ajax_laud(){
