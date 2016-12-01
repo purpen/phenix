638fea9804f970c639edbc9643ff1f8e7873ee6a
dafengniao <987083180@qq.com>
2016-12-01 16:07:27 +0800

    promo

--- src/Sher/Wap/Action/Promo.php
+++ src/Sher/Wap/Action/Promo.php
@@ -20,6 +20,14 @@ class Sher_Wap_Action_Promo extends Sher_Wap_Action_Base {
 	}
 
     public function lottery(){
+        //微信分享
+        $this->stash['app_id'] = Doggy_Config::$vars['app.wechat.app_id'];
+        $timestamp = $this->stash['timestamp'] = time();
+        $wxnonceStr = $this->stash['wxnonceStr'] = new MongoId();
+        $wxticket = Sher_Core_Util_WechatJs::wx_get_jsapi_ticket();
+        $url = $this->stash['current_url'] = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']; 
+        $wxOri = sprintf("jsapi_ticket=%s&noncestr=%s&timestamp=%s&url=%s", $wxticket, $wxnonceStr, $timestamp, $url);
+        $this->stash['wxSha1'] = sha1($wxOri);
         return $this->to_html_page('wap/promo/lottery.html');
     }
 
