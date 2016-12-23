e19a44f8b2674e33648f9395d4043f4c563bb2e2
tianshuai <tianshuaiorc@sina.com>
2016-12-23 09:19:33 +0800

    order show add summary

--- src/Sher/Core/Model/Product.php
+++ src/Sher/Core/Model/Product.php
@@ -338,12 +338,16 @@ class Sher_Core_Model_Product extends Sher_Core_Model_Base {
         'send_type' => 1,
         # 供应商
         'supplier_id' => null,
+        # 是否可推广: 0.否；1.是；
+        'is_commision' => 0,
+        # 佣金比例
+        'commision_percent' => 0,
 
     );
 	
 	protected $required_fields = array('user_id','title');
-	protected $int_fields = array('user_id','designer_id','category_id','inventory','sale_count','presale_count','presale_people', 'mode_count','appoint_count','state','published','deleted','process_voted','process_presaled','process_saled','presale_inventory','snatched_count','app_snatched_count','app_snatched_total_count','stuff_count','last_editor_id','max_bird_coin','min_bird_coin','exchange_count','app_snatched_limit_count','guide_id','hatched', 'app_category_id', 'pid', 'featured_on', 'stick_on', 'from_to', 'number','send_type');
-	protected $float_fields = array('cost_price', 'market_price', 'sale_price', 'hot_price', 'presale_money', 'presale_goals', 'snatched_price', 'app_snatched_price', 'exchange_price');
+	protected $int_fields = array('user_id','designer_id','category_id','inventory','sale_count','presale_count','presale_people', 'mode_count','appoint_count','state','published','deleted','process_voted','process_presaled','process_saled','presale_inventory','snatched_count','app_snatched_count','app_snatched_total_count','stuff_count','last_editor_id','max_bird_coin','min_bird_coin','exchange_count','app_snatched_limit_count','guide_id','hatched', 'app_category_id', 'pid', 'featured_on', 'stick_on', 'from_to', 'number','send_type','is_commision');
+	protected $float_fields = array('cost_price', 'market_price', 'sale_price', 'hot_price', 'presale_money', 'presale_goals', 'snatched_price', 'app_snatched_price', 'exchange_price','commision_percent');
 	protected $counter_fields = array('inventory','sale_count','presale_count', 'mode_count','asset_count', 'view_count', 'favorite_count', 'love_count', 'comment_count','topic_count','vote_favor_count','vote_oppose_count','appoint_count','stuff_count','exchange_count', 'app_appoint_count', 'true_view_count', 'web_view_count', 'wap_view_count', 'app_view_count');
 	protected $retrieve_fields = array('content'=>0);
 	protected $joins = array(
@@ -656,6 +660,11 @@ class Sher_Core_Model_Product extends Sher_Core_Model_Base {
         // 库存数量不为能负数
         if(isset($data['inventory']) && (int)$data['inventory'] < 0){
             $data['inventory'] = 0;
+        }
+
+        // 是否可推广
+        if(isset($data['commision_percent']) && !empty($data['commision_percent'])){
+            $data['is_commision'] = 1;
         }
 
         // 抢购库存数量不为能负数
