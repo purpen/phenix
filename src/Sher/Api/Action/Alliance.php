f11fe21b7de09da12c4e9fb5df9e2a79bf0161b9
tianshuai <tianshuaiorc@sina.com>
2017-01-20 10:40:00 +0800

    alliance

--- src/Sher/Api/Action/Alliance.php
+++ src/Sher/Api/Action/Alliance.php
@@ -5,7 +5,7 @@
  */
 class Sher_Api_Action_Alliance extends Sher_Api_Action_Base {
 	
-	protected $filter_user_method_list = array('execute');
+	protected $filter_user_method_list = array('execute', 'info');
 	
 	/**
 	 * 入口
@@ -53,6 +53,35 @@ class Sher_Api_Action_Alliance extends Sher_Api_Action_Base {
 
 		return $this->api_json('请求成功', 0, $data);
 	}
+
+    /**
+     * 规范
+     */
+    public function info(){
+        $str = "
+可提现金额：
+是指当前帐户可以提现的金额，您可以随时申请提现。
+
+我的收益：
+交易记录里的佣金状态变为“已结算”状态后，该笔订单商品对应的“佣金”会计入帐户的“可提现金额”。已结算收入会持续累加，即累计您所有已结算的金额。
+
+已提现金额：
+帐户以往提现金额的总和。
+
+交易记录里佣金状态何时会变为“已结算”？
+订单状态为“完成交易”（用户确认收货并发表评价）并且对产生佣金的订单无退款退货行为，佣金状态变更为“待结算”。系统会在第二天完成结算统计并生成相应的结算单，同时被结算的交易单更新为”已结算“。
+注：用户收货后无操作，系统默认自发货时间起至15日内完成交易。
+
+如何查看订单结算记录？
+你可以在结算记录里，查询结算交易的产品数量以及佣金，在结算明细里可以详细的看到佣金来源订单的商品。
+
+如何提现：
+您可以随时将可提现金额提现，每次提现须大于100元，提现需要1~2个工作日的人工审核，通过审核后，相应款项会打入您的提现账户。
+您可以在“提现记录”页面，查看每笔提现的状态。
+";
+		return $this->api_json('请求成功', 0, array('info'=>$str)); 
+    
+    }
 
 	
 }
