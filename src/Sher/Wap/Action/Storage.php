3f5d1daeec8cddc55d6b78807ef66791a0c6f111
tianshuai <tianshuaiorc@sina.com>
2017-02-22 09:27:42 +0800

    storage

--- src/Sher/Wap/Action/Storage.php
+++ src/Sher/Wap/Action/Storage.php
@@ -69,7 +69,7 @@ class Sher_Wap_Action_Storage extends Sher_Wap_Action_Base {
 	    $timestamp = $this->stash['timestamp'] = time();
 	    $wxnonceStr = $this->stash['wxnonceStr'] = new MongoId();
 	    $wxticket = Sher_Core_Util_WechatJs::wx_get_jsapi_ticket();
-	    $url = $this->stash['current_url'] = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']; 
+	    $url = $this->stash['current_url'] = 'https://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']; 
 	    $wxOri = sprintf("jsapi_ticket=%s&noncestr=%s&timestamp=%s&url=%s", $wxticket, $wxnonceStr, $timestamp, $url);
 	    $this->stash['wxSha1'] = sha1($wxOri);
 
