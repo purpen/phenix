8e3b85eb55c8f488b5aba2d1d0022ea07af609e3
tianshuai <tianshuaiorc@sina.com>
2017-03-04 12:31:03 +0800

    index d3in down

--- src/Sher/App/Action/Index.php
+++ src/Sher/App/Action/Index.php
@@ -314,7 +314,7 @@ class Sher_App_Action_Index extends Sher_App_Action_Base {
      * fiu 下载
      */
     public function fiu_download(){
-        $url = "http://frstatic.qiniudn.com/download/app-release_1.8.3.apk";
+        $url = "http://frstatic.qiniudn.com/download/app-release_d3in_1.0.apk";
         return $this->to_redirect($url);
     }
 
