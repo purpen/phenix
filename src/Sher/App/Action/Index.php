9f13d65aa1fb0d5f878bb32360adf27fe7d61128
tianshuai <tianshuaiorc@sina.com>
2017-04-26 17:36:10 +0800

    andorid d3in update

--- src/Sher/App/Action/Index.php
+++ src/Sher/App/Action/Index.php
@@ -317,7 +317,7 @@ class Sher_App_Action_Index extends Sher_App_Action_Base {
      * fiu 下载
      */
     public function fiu_download(){
-        $url = "http://frstatic.qiniudn.com/download/app-release_1.8.9.apk";
+        $url = "http://frstatic.qiniudn.com/download/app-release_1.9.0.apk";
         return $this->to_redirect($url);
     }
 
