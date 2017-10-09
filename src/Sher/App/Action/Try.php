ebd0be2c9c6eeb5c8319bfebb745ae47b47e5f6f
tianshuai <tianshuaiorc@sina.com>
2017-10-09 14:06:59 +0800

    header add topic

--- src/Sher/App/Action/Try.php
+++ src/Sher/App/Action/Try.php
@@ -22,8 +22,6 @@ class Sher_App_Action_Try extends Sher_App_Action_Base implements DoggyX_Action_
 	
 	public function _init() {
 		$this->set_target_css_state('page_sub_try');
-		$this->set_target_css_state('page_incubator');
-    $this->set_target_css_state('page_cooperate');
     }
 	
 	/**
