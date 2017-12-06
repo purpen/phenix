653abf0863709bbc3f93d4b550eb307212474499
tianshuai <tianshuaiorc@sina.com>
2017-12-06 17:40:13 +0800

    promo

--- src/Sher/Wap/Action/Promo.php
+++ src/Sher/Wap/Action/Promo.php
@@ -10,22 +10,13 @@ class Sher_Wap_Action_Promo extends Sher_Wap_Action_Base {
       'target_id'=>0,
     );
     
-    protected $exclude_method_list = array('execute', 'test', 'coupon', 'dreamk', 'chinadesign', 'momo', 'watch', 'year_invite','year','jd','xin','six','zp','zp_share','qixi','hy','din','request','rank', 'fetch_bonus','idea','idea_sign','draw','jdzn','common_sign','db_bonus','coin','coin_submit','hy_sign','rank2','comment_vote_share','sign','xy','mf','source','zces','holiday','hoshow','cappa','android_download','sign_app','zzces','send_bonus','fiu','load_up_img','ym','eleven','theme','fiuinvite','tshare','teeth','lottery','double','esthetics','intelligence','outdoor','clothes','receive_zongzi','receive_zongzi_ok','wx_article','wx_active','wx_try','wx_report','wx_product','wx_zc','wx_cooperate', 'wx_cooperate_success','qsyd3', 'd3ingo_zj','wx_proxy','wx_callback');
+    protected $exclude_method_list = array('execute', 'test', 'coupon', 'dreamk', 'chinadesign', 'momo', 'watch', 'year_invite','year','jd','xin','six','zp','zp_share','qixi','hy','din','request','rank', 'fetch_bonus','idea','idea_sign','draw','jdzn','common_sign','db_bonus','coin','coin_submit','hy_sign','rank2','comment_vote_share','sign','xy','mf','source','zces','holiday','hoshow','cappa','android_download','sign_app','zzces','send_bonus','fiu','load_up_img','ym','eleven','theme','fiuinvite','tshare','teeth','lottery','double','esthetics','intelligence','outdoor','clothes','receive_zongzi','receive_zongzi_ok','wx_article','wx_active','wx_try','wx_report','wx_product','wx_zc','wx_cooperate', 'wx_cooperate_success','qsyd3', 'd3ingo_zj','wx_proxy');
 
     /**
      * 网站入口
      */
     public function execute(){
       //return $this->coupon();
-    }
-
-    /**
-     * 微信授权回调
-     */
-    public function wx_callback() {
-      echo $_POST;
-      echo $_GET;
-    
     }
 
     /**
@@ -76,7 +67,7 @@ class Sher_Wap_Action_Promo extends Sher_Wap_Action_Base {
             $options = array(
                 $authUrl,
                 '?appid=' . $appid,
-                '&redirect_uri=' . urlencode($protocol . '://' . $_SERVER['HTTP_HOST'] . '/promo/wx_callback'),
+                '&redirect_uri=' . urlencode($protocol . '://' . $_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']),
                 '&response_type=code',
                 '&scope=' . $scope,
                 '&state=' . $state,
