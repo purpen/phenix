deab73846e89159be85c1c93a59f55f4e23451a9
tianshuai <tianshuaiorc@sina.com>
2018-09-18 17:32:03 +0800

    auth

--- src/Sher/App/Action/Auth.php
+++ src/Sher/App/Action/Auth.php
@@ -396,7 +396,7 @@ class Sher_App_Action_Auth extends Sher_App_Action_Base {
         $user_state = $result['state'];
         
         if ($user_state == Sher_Core_Model_User::STATE_BLOCKED) {
-            return $this->ajax_json('此帐号涉嫌违规已经被禁用!', true, '/');
+            return $this->ajax_json('此帐号涉嫌违规已经被禁用!', true);
         }
 
         $third_info = '';
