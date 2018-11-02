7bf9dd93a7f722001b4e9497218c226546f597a5
tianshuai <tianshuaiorc@sina.com>
2018-11-02 22:19:45 +0800

    util

--- src/Sher/Core/Helper/Util.php
+++ src/Sher/Core/Helper/Util.php
@@ -1451,13 +1451,13 @@ class Sher_Core_Helper_Util {
         curl_setopt($ch, CURLOPT_URL, $url);//抓取指定网页
         curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
-        curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         // curl_setopt($ch, CURLOPT_PORT, 8003); // 端口号
         if ($method === 'POST') {
             curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
             curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false);
         }
         $result = curl_exec($ch);//运行curl
         curl_close($ch);
